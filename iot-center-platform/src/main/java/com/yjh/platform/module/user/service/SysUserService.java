8843f21f62937d70c53a917cee640db1b1cb5603
taotao <taotao@yijiahe.com>
2020-08-14 14:44:51 +0800

    需求[#14450]: 用户管理后台接口开发
        [修改内容]:用户管理后台接口开发
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I3277376f65c4596b9e1d4c422b880c0a12135fc0
    Signed-off-by: taotao <taotao@yijiahe.com>

---
+++ iot-center-platform/src/main/java/com/yjh/platform/module/user/service/SysUserService.java
@@ -0,0 +1,155 @@
+package com.yjh.platform.module.user.service;
+
+import com.yjh.platform.common.result.BusinessException;
+import com.yjh.platform.common.utils.DateTimeUtil;
+import com.yjh.platform.module.device.entity.AreaInfo;
+import com.yjh.platform.module.user.entity.SysOrg;
+import com.yjh.platform.module.user.entity.SysUser;
+import com.yjh.platform.module.user.dao.SysUserDao;
+
+import java.util.*;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.yjh.platform.common.logs.Logs;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+* @author tt
+* @since 2020-07-23
+*/
+@Service
+public class SysUserService{
+
+    @Autowired
+    private SysUserDao sysUserDao;
+
+<<<<<<< Updated upstream
+    @Logs(title = "插入新用户", code = "sysUser")
+=======
+
+    @Logs(title = "插入", code = "module")
+>>>>>>> Stashed changes
+    @Transactional(rollbackFor = Exception.class)
+    public int insert(SysUser sysUser) {
+        Date date = new Date();
+        sysUser.setCreateTime(date);
+        sysUser.setUpdateTime(date);
+        return this.sysUserDao.insert(sysUser);
+    }
+
+    @Logs(title = "删除", code = "module")
+    @Transactional(rollbackFor = Exception.class)
+    public int deleteByPrimaryId(Long userId) {
+        return this.sysUserDao.deleteByPrimaryId(userId);
+    }
+
+    @Logs(title = "更新", code = "module")
+    @Transactional(rollbackFor = Exception.class)
+    public int update(SysUser sysUser) {
+        Date date = new Date();
+        sysUser.setUpdateTime(date);
+        return this.sysUserDao.update(sysUser);
+    }
+
+    @Logs(title = "主键查询", code = "module")
+    @Transactional(rollbackFor = Exception.class)
+    public SysUser selectByPrimaryId(Long userId) {
+        return this.sysUserDao.selectByPrimaryId(userId);
+    }
+
+    @Logs(title = "查询", code = "module")
+    @Transactional(rollbackFor = Exception.class)
+    public List<SysUser> select(Long userId, String userName, String password, String trueName, Integer userType, Integer sex, String eMail, String mobilePhone, String workNo, String faceId, String fingerId, String voiceId, Integer state, String userTitle, Long creatorId, String appkey, String imageUrl, Long roleId, Long orgId, Integer userStatus, Date createTime, Date updateTime, Date invalidTime, Date lastLogin) {
+        return sysUserDao.select(userId, userName, password, trueName, userType, sex, eMail, mobilePhone, workNo, faceId, fingerId, voiceId, state, userTitle, creatorId, appkey, imageUrl, roleId, orgId, userStatus, createTime, updateTime, invalidTime, lastLogin);
+    }
+
+    @Logs(title = "分页查询", code = "module")
+    @Transactional(rollbackFor = Exception.class)
+    public List<Map<String, String>> selectByPage(SysUser sysUser) {
+        if (sysUser.getUserStatus() != null && sysUser.getUserStatus()==-1) {
+            sysUser.setUserStatus(null);
+        }
+        return sysUserDao.selectByPage(sysUser);
+    }
+
+    @Logs(title = "根据用户ID查询关联的组织结构信息", code = "module")
+    @Transactional(rollbackFor = Exception.class)
+    public SysOrg selectRelationOrg(Long userId) {
+        return this.sysUserDao.selectRelationOrg(userId);
+    }
+
+    @Logs(title = "根据用户ID查询关联菜单权限", code = "module")
+    @Transactional(rollbackFor = Exception.class)
+    public List<String> selectRelationMenu(Long userId) {
+        return this.sysUserDao.selectRelationMenu(userId);
+    }
+
+    @Logs(title = "根据用户ID查询关联区域设备权限", code = "module")
+    @Transactional(rollbackFor = Exception.class)
+    public List<AreaInfo> selectRelationAuthor(Long userId) {
+        List<AreaInfo> areaInfoCountryList = new ArrayList<>();
+        List<Map<String, String>> list = this.sysUserDao.selectRelationAuthor(userId);
+        for(Iterator<Map<String, String>> it = list.iterator();it.hasNext();){
+            Map<String, String> areaInfoMap = it.next();
+            if (Objects.equals(areaInfoMap.get("upId"), null) || Objects.equals(areaInfoMap.get("upId"), "")) {
+                AreaInfo areaInfoCountry = new AreaInfo();
+                areaInfoCountry.setLabel(areaInfoMap.get("label"));
+                areaInfoCountry.setId(Long.valueOf(areaInfoMap.get("Id")));
+                areaInfoCountry.setInfoType(areaInfoMap.get("infoType"));
+                areaInfoCountry.setUpName(areaInfoMap.get("upName"));
+                areaInfoCountryList.add(areaInfoCountry);
+            }
+        }
+        diGui(areaInfoCountryList, list);
+        return areaInfoCountryList;
+    }
+
+    @Logs(title = "用户登录", code = "module")
+    @Transactional(rollbackFor = Exception.class)
+    public SysUser userLogin(String userName, String password) {
+        SysUser sysUser = this.sysUserDao.selectByUserName(userName);
+        if (!sysUser.getPassword().equals(password)) {
+            throw new BusinessException("用户密码错误");
+        } else {
+            Date date = new Date();
+            String appKey = String.valueOf(DateTimeUtil.getSecondTimestamp(date));
+            sysUser.setAppkey(appKey);
+        }
+        return sysUser;
+    }
+
+    @Logs(title = "用户登录", code = "module")
+    @Transactional(rollbackFor = Exception.class)
+    public int userLogout(String userId) {
+        SysUser sysUserParams = new SysUser();
+        sysUserParams.setLastLogin(new Date());
+        long userIdLong = Long.valueOf(userId);
+        sysUserParams.setUserId(userIdLong);
+        return this.sysUserDao.update(sysUserParams);
+    }
+
+    private void diGui(List<AreaInfo> areaInfoList, List<Map<String, String>> listTree) {
+        for(AreaInfo areaInfo : areaInfoList){
+            List<AreaInfo> childrenList = new ArrayList<>();
+            for(Iterator<Map<String, String>> it = listTree.iterator();it.hasNext();){
+                Map<String, String> areaInfoMap = it.next();
+                if (Objects.equals(areaInfo.getId(), areaInfoMap.get("upId"))) {
+                    AreaInfo areaInfoTem = new AreaInfo();
+                    areaInfoTem.setUpId(Long.valueOf(areaInfoMap.get("upId")));
+                    areaInfoTem.setId(Long.valueOf(areaInfoMap.get("Id")));
+                    areaInfoTem.setLabel(areaInfoMap.get("label"));
+                    areaInfoTem.setInfoType(areaInfoMap.get("infoType"));
+                    areaInfoTem.setUpName(areaInfoMap.get("upName"));
+                    childrenList.add(areaInfoTem);
+                }
+            }
+            if (childrenList.size()>0 ) {
+                areaInfo.setChildren(childrenList);
+                diGui(childrenList, listTree);
+            }
+        }
+    }
+
+}
+
