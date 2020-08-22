5ae608a942479e1f016bb0f79b4aeeb7f487611b
yangchen <yangchen@yijiahe.com>
2020-08-22 14:13:12 +0800

    需求[#15153]: 视频预置位配置管理接口开发
        [修改内容]:摄像头数据库字段有修改，代码需要修改
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I1ddb116d02ba1d8c92ab478d492aa7e270cc59d2

--- iot-center-platform/src/main/java/com/yjh/platform/module/user/service/TCameraInfoService.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/user/service/TCameraInfoService.java
@@ -5,6 +5,11 @@ import com.yjh.platform.module.user.dao.TCameraInfoDao;
 
 import java.util.List;
 
+<<<<<<< Updated upstream
+=======
+import com.yjh.platform.module.user.entity.TCameraInfoByDict;
+import com.yjh.platform.module.user.entity.TCameraPreset;
+>>>>>>> Stashed changes
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.yjh.platform.common.logs.Logs;
@@ -40,22 +45,42 @@ public class TCameraInfoService{
 
     @Logs(title = "主键查询", code = "module")
     @Transactional(rollbackFor = Exception.class)
-    public TCameraInfo selectByPrimaryId(Long cameraId) {
-        return this.tCameraInfoDao.selectByPrimaryId(cameraId);
+    public TCameraInfoByDict selectByPrimaryId(Long cameraId) {
+        TCameraInfoByDict tCameraInfoByDict = this.tCameraInfoDao.selectByPrimaryId(cameraId);
+        System.out.println("结果是："+tCameraInfoByDict);
+        return tCameraInfoByDict;
+//        return this.tCameraInfoDao.selectByPrimaryId(cameraId);
     }
 
+<<<<<<< Updated upstream
+=======
+    @Logs(title = "根据间隔id查询所有摄像机信息", code = "TStdRegion")
+    @Transactional(rollbackFor = Exception.class)
+    public List<TCameraInfoByDict> selectByRegionId(Long regionId) {
+        List<TCameraInfoByDict> tCameraInfoByDictList = tCameraInfoDao.selectByRegionId(regionId);
+        return tCameraInfoByDictList;
+    }
+
+    @Logs(title = "根据摄像机名称查询信息", code = "tCameraInfo")
+    @Transactional(rollbackFor = Exception.class)
+    public List<TCameraInfoByDict> selectByCameraName(String cameraName) {
+        List<TCameraInfoByDict> tCameraInfoByDictList = tCameraInfoDao.selectByCameraName(cameraName);
+        return tCameraInfoByDictList;
+    }
+
+>>>>>>> Stashed changes
     @Logs(title = "查询", code = "module")
     @Transactional(rollbackFor = Exception.class)
-    public List<TCameraInfo> select(Long cameraId, String cameraName, String aliasName, String recordId, Long upRegionId, Integer channelNum, Integer smsId, Integer rmsId, String factoryName, Integer streamType, Integer protocolType, String url, Integer port, Integer cameraType, Integer isControl) {
-        List<TCameraInfo> tCameraInfoList = tCameraInfoDao.select(cameraId, cameraName, aliasName, recordId, upRegionId, channelNum, smsId, rmsId, factoryName, streamType, protocolType, url, port, cameraType, isControl);
-        return tCameraInfoList;
+    public List<TCameraInfoByDict> select(Long cameraId, String cameraName, String aliasName, String recordId, Long upRegionId, Integer channelNum, Integer smsId, Integer rmsId, Integer vendorId, Integer streamType, Integer protocolType, String url, Integer port, Integer cameraType, Integer isControl) {
+        List<TCameraInfoByDict> tCameraInfoByDictList = tCameraInfoDao.select(cameraId, cameraName, aliasName, recordId, upRegionId, channelNum, smsId, rmsId, vendorId, streamType, protocolType, url, port, cameraType, isControl);
+        return tCameraInfoByDictList;
     }
 
     @Logs(title = "分页查询", code = "module")
     @Transactional(rollbackFor = Exception.class)
-    public List<TCameraInfo> selectByPage(TCameraInfo tCameraInfo) {
-        List<TCameraInfo> tCameraInfoList = tCameraInfoDao.selectByPage(tCameraInfo);
-        return tCameraInfoList;
+    public List<TCameraInfoByDict> selectByPage(TCameraInfo tCameraInfo) {
+        List<TCameraInfoByDict> tCameraInfoByDict = tCameraInfoDao.selectByPage(tCameraInfo);
+        return tCameraInfoByDict;
     }
 
 }
