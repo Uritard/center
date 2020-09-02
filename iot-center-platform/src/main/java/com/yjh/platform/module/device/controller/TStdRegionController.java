59ac297d4eb638519f86e568cfbd700bce221e37
taotao <taotao@yijiahe.com>
2020-09-02 10:15:46 +0800

    缺陷[#14887]: 【系统管理-组织区域】厂站区域，列表有数据，输入存在的关键字查询，无查询结果
        [修改内容]:【系统管理-组织区域】厂站区域，列表有数据，输入存在的关键字查询，无查询结果
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I70b488738d7294f3c09aa81a340b2156f3752499
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-platform/src/main/java/com/yjh/platform/module/device/controller/TStdRegionController.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/device/controller/TStdRegionController.java
@@ -1,6 +1,8 @@
 package com.yjh.platform.module.device.controller;
 
 import com.yjh.platform.common.result.BusinessException;
+import com.yjh.platform.module.device.entity.AreaInfo;
+import com.yjh.platform.module.device.service.TStdDeviceService;
 import com.yjh.platform.module.device.service.TStdRegionService;
 import com.yjh.platform.module.device.entity.TStdRegion;
 import java.util.HashMap;
@@ -31,6 +33,9 @@ public class TStdRegionController {
 
     @Autowired
     private final TStdRegionService tStdRegionService;
+
+    @Autowired
+    private TStdDeviceService tStdDeviceService;
 
     private Logger log = LoggerFactory.getLogger(TStdRegionController.class);
 
@@ -146,7 +151,8 @@ public class TStdRegionController {
     public Result selectRegTreeByRegName(@RequestParam(value = "regionName", required = false) String regionName) {
         Result result = new Result();
         try {
-            result.setData(tStdRegionService.selectRegTreeByRegName(regionName));
+            List<AreaInfo> devTreeList = tStdDeviceService.selectRegionTreeByName(regionName);
+            result.setData(devTreeList);
         } catch (Exception e) {
             result.setCode(ResultCodeEnum.UPDATEERROR.getCode(), ResultCodeEnum.UPDATEERROR.getName());
             log.error("区域树模糊查询失败：", e);
