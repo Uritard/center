a4e5b5ba28a73ecd3576742f6536d20510113cf3
changzuhao <changzuhao@yijiahe.com>
2020-09-04 09:10:55 +0800

    需求[#15094] AR-20200706-08-0201 巡视监控接口     "[修改内容]: 智能巡视-巡视监控业务接口开发     [描述说明]:获取当前任务下巡检点状态结果信息，任务进度显示，获取当前任务运行累计时间，获取当前任务不同巡检结果状态的巡检点数量     [问题原因]:     [解决方法]:     [测试建议]:  "
    Change-Id: I99bfa0fcbb8808829cdc0bde787dbc40d0443d84
    Signed-off-by: changzuhao <changzuhao@yijiahe.com>

--- iot-center-platform/src/main/java/com/yjh/platform/module/task/service/TCruiseTaskResultService.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/task/service/TCruiseTaskResultService.java
@@ -2,10 +2,14 @@ package com.yjh.platform.module.task.service;
 
 import com.alibaba.druid.util.StringUtils;
 import com.google.common.collect.Sets;
+import com.yjh.platform.module.task.entity.CruiseResultCounter;
+import com.yjh.platform.module.task.entity.CruiseTaskRunningTime;
 import com.yjh.platform.module.task.entity.TCruiseTaskResult;
 import com.yjh.platform.module.task.dao.TCruiseTaskResultDao;
 
 import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import org.springframework.data.redis.core.RedisCallback;
@@ -102,18 +106,155 @@ public class TCruiseTaskResultService{
             return keys;
         });
     }
-    @Logs(title = "获取当前任务的巡检点执行信息")
+    @Logs(title = "获取当前任务的巡检点全量信息 (巡检点初始化查询)")
     @Transactional(rollbackFor = Exception.class)
     public List<Map> selectCruiseTaskResult(Long taskId){
+        //获取数据库键名列表
         Set<String> keyResult=redisScan("t_cruise_task_result*");
-
+        //创建容纳结果信息的容器
         List<Map> dataResult=new ArrayList<>();
         for(String key:keyResult){
-            Map<String,Object> resultMap=redisTemplate.opsForHash().entries(key);
-            dataResult.add(resultMap);
+            Map<String,Object> resultMap=redisTemplate.opsForHash().entries(key);//循环每个键名取相应的键值对数据
+            Object values=resultMap.get("taskId");//取出每条数据的key值为“taskId”的value值
+            String value=values.toString();
+            String TaskId=taskId.toString(); //转化成统一格式进行比较筛选
+            if(TaskId.equals(value)&&resultMap.get("cruiseStatus").equals("1")){
+                dataResult.add(resultMap);
+            }
+
         }
         return dataResult;
     }
 
+
+    @Logs(title = "获取当前任务的巡检点结果信息(巡检点结果)")
+    @Transactional(rollbackFor = Exception.class)
+    public List<Map> selectCurrentCruiseTaskResult(Long taskId,Long cruiseId,List<Map> cruiseResult){
+        //获取数据库键名列表
+        Set<String> keyResult=redisScan("t_cruise_task_result*");
+        //创建容纳结果信息的容器
+        List<Map> dataResult=cruiseResult;
+         for(String key:keyResult){
+             Map<String,Object> resultMap=redisTemplate.opsForHash().entries(key);//循环每个键名取相应的键值对数据
+             String value1=(resultMap.get("taskId")).toString();//取出每条数据的key值为“taskId”的value值
+             String value2=(resultMap.get("cruiseId").toString());
+             String TaskId=taskId.toString();
+             String CruiseId=cruiseId.toString();//转化成统一格式进行比较筛选
+             if(TaskId.equals(value1)&&CruiseId.equals(value2)&&resultMap.get("cruiseStatus").equals("0")){  //巡检完成数据插入结果集
+                 dataResult.add(resultMap);
+             }else if (TaskId.equals(value1)&&CruiseId.equals(value2)&&resultMap.get("cruiseStatus").equals("1")){ //删除对应的未完成信息
+                 dataResult.remove(resultMap);
+             }
+
+         }
+
+
+        return dataResult;
+    }
+
+
+    @Logs(title = "统计获取当前任务的执行进度")
+    @Transactional(rollbackFor = Exception.class)
+    public float selectCruiseAdvance(Long taskId){
+        Set<String> keyResult=redisScan("t_cruise_task_result*");
+        float cruiseCount=0;//总巡检点数量
+        float cruiseComCount=0;//执行完成的巡检点数量
+       for (String keys:keyResult){
+           Map<String,Object> resultMap=redisTemplate.opsForHash().entries(keys);
+           Object values=resultMap.get("taskId");//取出每条数据的key值为“taskId”的value值
+           String value=values.toString();
+           String TaskId=taskId.toString(); //转化成统一格式进行比较筛选
+           if(TaskId.equals(value)&&resultMap.get("cruiseStatus").equals("1")){
+              cruiseCount=cruiseCount+1;
+           }else if (TaskId.equals(value)&&resultMap.get("cruiseStatus").equals("0")){
+               cruiseComCount=cruiseComCount+1;
+           }
+       }
+       return cruiseComCount/cruiseCount;
+    }
+
+
+    @Logs(title = "查询当前任务下巡检点各个状态下的数量")
+    @Transactional(rollbackFor = Exception.class)
+    public CruiseResultCounter selectCruiseStatusCount(Long taskId){
+        Set<String>keyResult=redisScan("t_cruise_task_result*");
+        CruiseResultCounter cruiseResultCounter=new CruiseResultCounter();
+        Integer abnormalCount=0;
+        Integer cruisedCount=0;
+        Integer cruiseCount=0;
+        for(String keys:keyResult){
+            Map<String,Object> resultMap=redisTemplate.opsForHash().entries(keys);
+            String value=(resultMap.get("taskId")).toString();
+            String TaskId=taskId.toString();
+            if(TaskId.equals(value)&&resultMap.get("cruiseResult").equals("1")){
+                    abnormalCount=abnormalCount+1;
+                    cruisedCount=cruisedCount+1;
+            }else if(TaskId.equals(value)&&resultMap.get("cruiseStatus").equals("0")){
+                   cruisedCount=cruisedCount+1;
+            }else if (TaskId.equals(value)&&resultMap.get("cruiseStatus").equals("1")){
+                cruiseCount=cruiseCount+1;
+            }
+
+            }
+        Integer cruiseNotCount=cruiseCount-cruisedCount;
+        cruiseResultCounter.setAbnormalCount(abnormalCount);
+        cruiseResultCounter.setCruisedCount(cruisedCount);
+        cruiseResultCounter.setCruiseNotCount(cruiseNotCount);
+
+        return cruiseResultCounter;
+    }
+
+    @Logs(title = "获取当前任务已运行的时间")
+    @Transactional(rollbackFor = Exception.class)
+    public CruiseTaskRunningTime selectTaskRunningTime(Long taskId) throws ParseException {
+
+        Set<String>keyResult=redisScan("t_cruise_task_result*");
+        CruiseTaskRunningTime cruiseTaskRunningTime=new CruiseTaskRunningTime();//创建时间对象
+        String startTime="yyyy-mm-dd HH:MM:SS";
+        //获取任务开始时间
+        for(String keys:keyResult){
+           Map<String,Object>mapResult=redisTemplate.opsForHash().entries(keys);
+            String value=(mapResult.get("taskId")).toString();
+            String TaskId=taskId.toString();
+            if(TaskId.equals(value)&&mapResult.get("cruiseStatus").equals("1")){
+                startTime=mapResult.get("cruiseTaskTime").toString();
+            }
+
+        }
+        //获取当前实时时间
+        Date date=new Date();
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String nowTime =simpleDateFormat.format(date);
+        Date d1 =simpleDateFormat.parse(startTime);
+        Date d2=simpleDateFormat.parse(nowTime);//将两个时间字符串转为日期类型
+        Long RunningTime=d2.getTime()-d1.getTime();//计算时间差
+        Long Day=RunningTime/(24*60*60*1000);
+        Long Hour=(RunningTime/(60*60*1000)-Day*24);
+        Long Minute=((RunningTime/(60*1000))-Day*24*60-Hour*60);
+        Long Second=(RunningTime/1000-Day*24*60*60-Hour*60*60-Minute*60);//将时间差转化为天时分秒格式
+
+        cruiseTaskRunningTime.setDay(Day);
+        cruiseTaskRunningTime.setHour(Hour);
+        cruiseTaskRunningTime.setMinute(Minute);
+        cruiseTaskRunningTime.setSeconds(Second);
+
+        return cruiseTaskRunningTime;
+    }
+
+    @Logs(title = "获取执行当前任务的机器人信息及相应巡检点结果状态")
+    @Transactional(rollbackFor = Exception.class)
+    public List<Map> selectRobotAndCruiseResult(Long taskId){
+        Set<String> robotKeys=redisScan("robot_info");
+        List<Map> robotResult=new ArrayList<>();
+        for(String key:robotKeys){
+            Map<String,Object> robotInfo=redisTemplate.opsForHash().entries(key);
+            String value=robotInfo.get("taskId").toString();
+            String TaskId=taskId.toString();
+            if(TaskId.equals(value)){
+                robotResult.add(robotInfo);
+            }
+        }
+        return robotResult;
+    }
 }
 
