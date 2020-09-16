4a036f49841a56b980980d53a75810246fe21d7c
taotao <taotao@yijiahe.com>
2020-09-16 21:09:46 +0800

    需求[#15298]: AR-20200706-08-0101 任务管理接口开发
        [修改内容]: AR-20200706-08-0101 任务管理接口开发
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: Icc8a2956eec193ad6879e8c5488ca34c460e3933
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-platform/src/main/java/com/yjh/platform/datasource/ManageDataSourceConfig.java
+++ iot-center-platform/src/main/java/com/yjh/platform/datasource/ManageDataSourceConfig.java
@@ -105,7 +105,7 @@ public class ManageDataSourceConfig {
         Properties p = new Properties();
         p.setProperty("offsetAsPageNum", "true");
         p.setProperty("rowBoundsWithCount", "true");
-        p.setProperty("reasonable", "true");
+        p.setProperty("reasonable", "false");
         //通过设置pageSize=0或者RowBounds.limit = 0就会查询出全部的结果。
         //p.setProperty("pageSizeZero", "true");
         //配置mysql数据库的方言
