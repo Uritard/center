b3365b34829aeb4daca4aef91e9b79a4963aa6c4
taotao <taotao@yijiahe.com>
2020-10-12 20:52:00 +0800

    需求[#17591]: AR-20200914-01-03 视频接入相机控制功能开发
        [修改内容]: AR-20200914-01-03 视频接入相机控制功能开发
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: If5df55e6bb6c1b72e727a1e5ea6f3f9c0c93efad
    Signed-off-by: taotao <taotao@yijiahe.com>

---
+++ iot-center-device/src/main/java/com/yjh/device/module/device/service/SysLogsService.java
@@ -0,0 +1,25 @@
+package com.yjh.device.module.device.service;
+
+import com.yjh.device.module.device.dao.SysLogsDao;
+import com.yjh.device.module.device.entity.SysLogs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+* @author tt
+* @since 2020-08-12
+*/
+@Service
+public class SysLogsService{
+
+    @Autowired
+    private SysLogsDao sysLogsDao;
+
+    @Transactional(rollbackFor = Exception.class)
+    public SysLogs selectByPrimaryId(String logId) {
+        return this.sysLogsDao.selectByPrimaryId(logId);
+    }
+
+}
+
