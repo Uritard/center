15586183ee3644f7135ef14c32a456c7aa23ee8d
changzuhao <changzuhao@yijiahe.com>
2020-11-12 16:26:30 +0800

    缺陷[#18884]【智能巡视-巡视监控】当前有正在进行的任务，查看巡视监控页面，提示更新数据失败
        [修改内容]: 优化任务数据集
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I10c2d415ee40d23aeafb15c1e101b9c26b6a1f36

--- iot-center-platform/src/main/java/com/yjh/platform/module/task/entity/TaskSimpleInfo.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/task/entity/TaskSimpleInfo.java
@@ -23,11 +23,9 @@ public class TaskSimpleInfo implements Serializable {
     @ApiModelProperty(value = "任务类型")
     private Integer Type;
     @ApiModelProperty(value = "任务状态")
-    private Integer cState;
+    private Integer taskState;
     @ApiModelProperty(value = "任务类型名称")
     private String TypeName;
-    @ApiModelProperty(value = "任务来源")
-    private Integer taskType;
-    @ApiModelProperty(value = "任务来源名称")
-    private String taskTypeName;
+    @ApiModelProperty(value = "任务状态名称")
+    private String taskStateName;
 }
