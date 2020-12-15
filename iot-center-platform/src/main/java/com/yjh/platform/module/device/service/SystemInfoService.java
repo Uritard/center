4090b2260d634240737dbd23b67d97f23e16fe8b
taotao <taotao@yijiahe.com>
2020-12-15 20:29:10 +0800

    需求[#21647]: SR-20200914-01-10 视频B接口开发-视频回放增加flv播放流
        [修改内容]: SR-20200914-01-10 视频B接口开发-视频回放增加flv播放流
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: Ie75accd7db07eca5c8794dec7f5ee7505b3096d9
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-platform/src/main/java/com/yjh/platform/module/device/service/SystemInfoService.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/device/service/SystemInfoService.java
@@ -32,7 +32,7 @@ public class SystemInfoService {
 
     private Logger log = LoggerFactory.getLogger(SystemInfoService.class);
 
-    @Logs(title = "获取内存信息", code = "module",content = "获取内存信息")
+//    @Logs(title = "获取内存信息", code = "module",content = "获取内存信息")
     @Transactional(rollbackFor = Exception.class)
     public Map<String,String> getMemory() throws Exception {
         return systemInfoUtil.getMemUsage();
@@ -52,7 +52,7 @@ public class SystemInfoService {
         }
         return result2;
     }
-    @Logs(title = "获取磁盘信息", code = "module",content = "获取磁盘信息")
+//    @Logs(title = "获取磁盘信息", code = "module",content = "获取磁盘信息")
     @Transactional(rollbackFor = Exception.class)
     public List<Map<String,String>> getSwap() throws Exception {
         List<Map<String,String>> result = systemInfoUtil.getDeskUsage();
@@ -65,20 +65,20 @@ public class SystemInfoService {
         return result2;
     }
 
-    @Logs(title = "获取cpu利用率", code = "module",content = "获取cpu利用率")
+//    @Logs(title = "获取cpu利用率", code = "module",content = "获取cpu利用率")
     @Transactional(rollbackFor = Exception.class)
     public double getCpuOnUse() throws Exception {
         return systemInfoUtil.getCpuOnUse();
     }
 
-    @Logs(title = "获取硬盘利用率", code = "module",content = "获取硬盘利用率")
+//    @Logs(title = "获取硬盘利用率", code = "module",content = "获取硬盘利用率")
     @Transactional(rollbackFor = Exception.class)
     public Map<String,Object> getDeskOnUse() throws Exception {
         return systemInfoUtil.getDeskOnUse();
     }
 
 
-    @Logs(title = "获取关键服务", code = "module",content = "获取关键服务")
+//    @Logs(title = "获取关键服务", code = "module",content = "获取关键服务")
     @Transactional(rollbackFor = Exception.class)
     public List<Map<String,Object>> getServices() throws Exception {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
