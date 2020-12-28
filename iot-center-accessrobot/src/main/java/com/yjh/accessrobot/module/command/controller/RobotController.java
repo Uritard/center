ae4a586c361597e4da0e623042a10030b7f3106a
yangchen <yangchen@yijiahe.com>
2020-12-28 09:29:11 +0800

    需求[#19778] SR-20201101-01-06 机器人对接--A接口模型同步指令实现
        [修改内容]:机器人对接--A接口模型同步指令实现
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: Ia15624205003d1428a2d3e736fc83fa038c2b992

--- iot-center-accessrobot/src/main/java/com/yjh/accessrobot/module/command/controller/RobotController.java
+++ iot-center-accessrobot/src/main/java/com/yjh/accessrobot/module/command/controller/RobotController.java
@@ -56,7 +56,7 @@ public class RobotController {
     }
     @ApiOperation(value = "发送模型同步指令接口")
     @RequestMapping(value = "/fileTransfer", method = RequestMethod.GET)
-    public Result feignRobotTransfer(@RequestParam(value = "type") String robotCode) {
+    public Result feignRobotTransfer(@RequestParam(value = "robotCode") String robotCode) {
         Result result = new Result();
         try {
             result.setData(robotService.feignRobotTransfer(robotCode));
