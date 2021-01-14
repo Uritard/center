0fee2a61a8860d8f7f0604eadd436637ea5fe72f
taotao <taotao@yijiahe.com>
2021-01-14 15:22:50 +0800

    需求[#22927]: SR-20210111-01-05 智慧变电安全功能开发--系统恢复功能
        [修改内容]: SR-20210111-01-05 智慧变电安全功能开发--系统恢复功能
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: Ib14cb47454684168301b98e29b952c47eb7e0e64
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-platform/src/main/java/com/yjh/platform/module/user/controller/TCameraPresetController.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/user/controller/TCameraPresetController.java
@@ -47,7 +47,7 @@ public class TCameraPresetController {
 
     @ApiOperation(value = "插入")
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public Result insert(@RequestBody @Validated TCameraPreset tCameraPreset) {
+    public Result insert(@RequestBody TCameraPreset tCameraPreset) {
         Result result = new Result();
         try {
             int resultNum = 0;
