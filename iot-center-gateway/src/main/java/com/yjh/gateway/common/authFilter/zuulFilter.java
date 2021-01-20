7fe9c276637c804aa6aff76517b377dbb2032820
taotao <taotao@yijiahe.com>
2021-01-20 21:17:21 +0800

    需求[#15376]: AR-20200707-02-04 统一分发鉴权网关搭建(gateway)
        [修改内容]: AR-20200707-02-04 统一分发鉴权网关搭建(gateway)
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: Id6200e322153b965c3b681e123ef3fbc3d738580
    Signed-off-by: taotao <taotao@yijiahe.com>

---
+++ iot-center-gateway/src/main/java/com/yjh/gateway/common/authFilter/zuulFilter.java
@@ -0,0 +1,37 @@
+package com.yjh.gateway.common.authFilter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class zuulFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(zuulFilter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String s = String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString());
+        log.info(s);
+        return null;
+    }
+}
