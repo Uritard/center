34b08eebbfc0c3d279fa7b539145eb7aa23aacf3
taotao <taotao@yijiahe.com>
2021-01-26 19:49:43 +0800

    缺陷[#23420]: 【智能巡视-任务管理】点击日历查看任务数据，删除整个周期任务，提示系统异常，再次删除可正常删除
        [修改内容]: 【智能巡视-任务管理】点击日历查看任务数据，删除整个周期任务，提示系统异常，再次删除可正常删除
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I8db36c40b56b9525e0ca36046ffb139d9c83954f
    Signed-off-by: taotao <taotao@yijiahe.com>

---
+++ iot-center-gateway/src/main/java/com/yjh/gateway/common/Hytrisx/UniVerFallback.java
@@ -0,0 +1,81 @@
+package com.yjh.gateway.common.Hytrisx;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+@Component
+public class UniVerFallback implements FallbackProvider {
+
+    /**
+     * 指定 可以熔断拦截 哪些服务
+     * @return
+     */
+    @Override
+    public String getRoute() {
+//        return "springcloud-ms-member";//指定 可熔断某个服务 服务名是配置文件中配置的各服务的serviceId
+        return "*"; //指定    可熔断所有服务
+    }
+
+    /**
+     * 指定  熔断后返回的定制化内容
+     * @param route
+     * @param cause
+     * @return
+     */
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            /**
+             * 设置响应体
+             * @return
+             * @throws IOException
+             */
+            @Override
+            public InputStream getBody() throws IOException {
+                //TODO 此处可以做日志记录
+                return new ByteArrayInputStream(JSON.toJSONString("服务凉凉了").getBytes());
+            }
+
+            /**
+             * 设置响应头信息
+             * @return
+             */
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON_UTF8);//指定响应体 格式+编码方式
+                return headers;
+            }
+        };
+    }
+}
--- iot-center-platform/src/main/java/com/yjh/platform/module/task/service/TCruiseTaskService.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/task/service/TCruiseTaskService.java
@@ -229,7 +229,8 @@ public class TCruiseTaskService {
                 return tCruiseTaskDelDao.insert(tCruiseTaskDel);
             }else {
                 //删除整个周期任务
-                for (ConcurrentHashMap<String,Object> mapItem: Constant.taskMap) {
+                for(Iterator<ConcurrentHashMap<String,Object>> it = Constant.taskMap.iterator();it.hasNext();){
+                    ConcurrentHashMap<String,Object> mapItem = it.next();
                     //找到任务Id
                     if(mapItem.get("taskId").equals(taskId)){
                         JobManager.removeJob(mapItem.get("jobName").toString(),mapItem.get("jobGroupName").toString(),mapItem.get("triggerName").toString(),mapItem.get("triggerGroupName").toString());
