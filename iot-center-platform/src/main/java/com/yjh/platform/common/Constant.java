8e914a04767221d472d531945cb434e8e1ffb35e
yangchen <yangchen@yijiahe.com>
2021-01-27 09:39:52 +0800

    需求[#19776]: SR-20201101-01-04 机器人对接--A接口控制指令实现
        [修改内容]:  机器人A接口完善和补充
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I13d63f929c7c09aff8dbe93528b587968aec8dc3

--- iot-center-platform/src/main/java/com/yjh/platform/common/Constant.java
+++ iot-center-platform/src/main/java/com/yjh/platform/common/Constant.java
@@ -52,12 +52,9 @@ public class Constant {
     public static final String TCP_URL = "http://iot-center-accesstcp/sendToUpSystem/v1/sendXML";
 
     public static Map<String,String> userInfo= new HashMap<>();
-
-<<<<<<< Updated upstream
     public static final String START_CAMERA_URL = "http://iot-center-accessvideo/camera/v1/startRealPlay?cameraId={cameraId}";
     public static final String START_ROBOT_CAMERA_URL = "http://iot-center-accessvideo/camera/v1/robotStartRealPlay?robotId={robotId}";
-=======
-    public static final String Maintenance_Issued = "http://iot-center-accessrobot2/robot/v1/deviceMaintenanceIssued";
->>>>>>> Stashed changes
+
+    public static final String Maintenance_Issued = "http://iot-center-accessrobot/robot/v1/deviceMaintenanceIssued";
 
 }
