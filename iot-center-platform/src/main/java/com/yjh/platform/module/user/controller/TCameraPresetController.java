f725022cfb5c930b9ccb49a9fd710ec256d35ffd
lqh <liangqinghua@yijiahe.com>
2021-02-07 13:12:06 +0800

    需求[#23831]: SR-20210103-02-47 音频分析-音频分析接口
        [描述说明]:  音频分析-音频分析接口
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I7b4b8c301c07ea27dde4fa847df3e804e53bd24c

--- iot-center-platform/src/main/java/com/yjh/platform/module/user/controller/TCameraPresetController.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/user/controller/TCameraPresetController.java
@@ -111,13 +111,13 @@ public class TCameraPresetController {
 
                 params.put("cameraId", tCameraPreset.getCameraId());
                 params.put("presetId", tCameraPreset.getPresetId());
-                Result response = sendPostRequest(Constant.CANCEL_PRESET_URL, params);
+                //Result response = sendPostRequest(Constant.CANCEL_PRESET_URL, params);
 
                 //int resultNum = 0;
                 //操作数据库
-                if (response.getData().equals(true)) {
-                    log.info("nvr删除预置位成功");
-                }
+//                if (response.getData().equals(true)) {
+//                    log.info("nvr删除预置位成功");
+//                }
                 //resultNum = tCameraPresetService.deleteByPrimaryId(presetId);
                 result.setData(resultNum);
             }
