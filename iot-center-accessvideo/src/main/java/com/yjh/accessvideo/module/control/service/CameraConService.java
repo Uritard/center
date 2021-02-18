7c965e49c272c95e856ed7fea594d75007d4cb73
taotao <taotao@yijiahe.com>
2021-02-18 14:49:18 +0800

    需求[#23881]: SR-20200914-01-14 视频B接口开发-视频流播放取消重复流
        [修改内容]: SR-20200914-01-14 视频B接口开发-视频流播放取消重复流
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I602b8acc6f94110c8c5aef5041c5b6111c9a8674
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/service/CameraConService.java
+++ iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/service/CameraConService.java
@@ -158,7 +158,7 @@ public class CameraConService {
             Integer processNum = Integer.parseInt(dataBackForId.substring(9,15).replace(" ",""));
             urlStop = "kill -9 "+processNum;
             Runtime.getRuntime().exec(urlStop);
-            redisTemplate.opsForHash().delete("cameraRealFlow:"+cameraId);
+            redisTemplate.delete("cameraRealFlow:"+cameraId);
             Constant.mapsForCamera.remove(String.valueOf(cameraId));
         } catch (Exception e) {e.getMessage();}
         return "stop " + cameraId + " preview success!";
