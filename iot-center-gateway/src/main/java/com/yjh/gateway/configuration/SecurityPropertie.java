c22682273f73a37e6b7e378e607cb7d26cc438c8
yuangang <yuangang@yijiahe.com>
2021-02-24 10:59:47 +0800

    需求[#23192]: SR-20210111-01-19 智慧变电安全功能开发--安全标记功能 [修改内容]:  SR-20210111-01-19 智慧变电安全功能开发--安全标记功能 [描述说明]: [问题原因]: [解决方法]: [测试建议]:
    Change-Id: I27dc90d2e4836e7f81291416b61cc06a530cf7ec
    Signed-off-by: yuangang <yuangang@yijiahe.com>

--- iot-center-gateway/src/main/java/com/yjh/gateway/configuration/SecurityPropertie.java
+++ iot-center-gateway/src/main/java/com/yjh/gateway/configuration/SecurityPropertie.java
@@ -21,6 +21,7 @@ public class SecurityPropertie implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         this.getIsCode();
+        this.getIsUkey();
     }
 
     /**
