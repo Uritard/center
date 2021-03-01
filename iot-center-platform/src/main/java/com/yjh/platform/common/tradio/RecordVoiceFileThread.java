848f8c654c87212bfdda6ef1940a59bd00ffc432
taotao <taotao@yijiahe.com>
2021-03-01 09:41:45 +0800

    需求[#24177]: SR-20210103-02-49 音频分析-音频wav文件获取
        [修改内容]: SR-20210103-02-49 音频分析-音频wav文件获取
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I66c760e0b74009d1986839e494054b1e3e8d75c2
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-platform/src/main/java/com/yjh/platform/common/tradio/RecordVoiceFileThread.java
+++ iot-center-platform/src/main/java/com/yjh/platform/common/tradio/RecordVoiceFileThread.java
@@ -96,7 +96,7 @@ public class RecordVoiceFileThread implements Runnable {
                             Integer channelNumTerm = Integer.parseInt(channelNumTem)-1;
                             if (channel == channelNumTerm) {
                                 try {
-                                    File file = new File(voicePath);
+                                    File file = new File(voicePath+voiceDeviceId+"/"+channelNumTem+"/"+new SimpleDateFormat("yyyy-MM-dd").format(new Date(dateTime)));
                                     if (!file.exists()) { file.mkdirs(); }
                                     File tempWav = new File(file, voiceName + "0" + Integer.parseInt(channelNumTem) +".aac");
                                     if (!tempWav.exists()) try { tempWav.createNewFile(); } catch (IOException e) { e.printStackTrace(); }
