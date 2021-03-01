658ed74b18bb1ef78817e6bd5c33380c46bdb39a
yuangang <yuangang@yijiahe.com>
2021-03-01 17:27:02 +0800

    需求[#23148]: SR-20210111-01-18 智慧变电安全功能开发--登录用户的会话防重放 [修改内容]:  SR-20210111-01-18 智慧变电安全功能开发--登录用户的会话防重放 [描述说明]: [问题原因]: [解决方法]: [测试建议]:
    Change-Id: If324b4960dcadf726a0c03067ccdce4187e26ec3
    Signed-off-by: yuangang <yuangang@yijiahe.com>

--- iot-center-gateway/src/main/java/com/yjh/gateway/common/authFilter/zuulFilter.java
+++ iot-center-gateway/src/main/java/com/yjh/gateway/common/authFilter/zuulFilter.java
@@ -15,6 +15,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -46,49 +50,64 @@ public class zuulFilter extends ZuulFilter {
     @Override
     public boolean shouldFilter() {
         Map<String, String> map = redisTemplate.opsForHash().entries("t_sys_param:isDecode");
-        String isDecode = map.get("content");
+        String isDecode =map.get("content");
         Map<String, String> uKeymap = redisTemplate.opsForHash().entries("t_sys_param:isUkey");
         String isUkey = uKeymap.get("content");
         if ("true".equals(isDecode)) {
             RequestContext ctx = RequestContext.getCurrentContext();
             HttpServletRequest request = ctx.getRequest();
             MyRequestWrapper requestWrapper = null;
+            String webcode = request.getHeader("summary")!= null ? request.getHeader("summary") : "";
             if (request instanceof HttpServletRequest) {
-                requestWrapper = new MyRequestWrapper(request);
                 if ("POST".equals(request.getMethod().toUpperCase()) || "PUT".equals(request.getMethod().toUpperCase())) {
-                    String webcode = null;
-                    String body = requestWrapper.getBody();
-                    Map<String, Object> paramBodyMap = new LinkedHashMap<>();
-                    JSONObject jsonModel = new JSONObject(true);
-                    if (!StringUtils.isEmpty(body)) {
-                        paramBodyMap = JSONObject.parseObject(body, LinkedHashMap.class);
-                    }
-                    for (Map.Entry<String, Object> param : paramBodyMap.entrySet()) {
-                        if (!"summary".equals(param.getKey()) && !"signStr".equals(param.getKey())) {
-                            jsonModel.put(param.getKey(), param.getValue());
-                        }
-                        if ("summary".equals(param.getKey())) {
-                            webcode = String.valueOf(param.getValue());
-                        }
-                    }
-                    if (jsonModel.size() != 0) {
-                        StringBuilder sb = new StringBuilder();
-                        if (null != jsonModel.toJSONString()) {
-                            for (char c : jsonModel.toJSONString().toCharArray()) {
-                                sb.append(String.valueOf((int) c));
+                    String contentType = request.getContentType();
+                        if (contentType != null && contentType.contains("multipart/form-data")) {
+//                            MultipartResolver resolver=new CommonsMultipartResolver(request.getSession().getServletContext());
+//                            MultipartHttpServletRequest multipartHttpServletRequest=resolver.resolveMultipart(request);
+//                            System.out.println("type:  " + multipartHttpServletRequest.getContentType());
+//                            MultipartFile multipartFile = multipartHttpServletRequest.getFile("file");
+//                            StringBuilder sb = new StringBuilder();
+//                            if (null != multipartFile) {
+//                                for (char c :  multipartFile.getInputStream().toString().toCharArray()) {
+//                                    sb.append(String.valueOf((int) c));
+//                                }
+//                            }
+//                            String token = Demo.summary(sb.toString());
+//                            if (!token.equals(webcode)) {
+//                                log.error("参数篡改" +multipartFile + " ,之后的summary: " + token + ",前端summary: " + webcode);
+//                                ctx.setSendZuulResponse(false);
+//                                ctx.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
+//                                return false;
+//                            }
+                        }else {
+                            requestWrapper = new MyRequestWrapper(request);
+                            String body = requestWrapper.getBody();
+                            Map<String, Object> paramBodyMap = new LinkedHashMap<>();
+                            JSONObject jsonModel = new JSONObject(true);
+                            if (!StringUtils.isEmpty(body)) {
+                                paramBodyMap = JSONObject.parseObject(body, LinkedHashMap.class);
                             }
+                            for (Map.Entry<String, Object> param : paramBodyMap.entrySet()) {
+                                jsonModel.put(param.getKey(), param.getValue());
+                            }
+                            if (jsonModel.size() != 0) {
+                                StringBuilder sb = new StringBuilder();
+                                if (null != jsonModel.toJSONString()) {
+                                    for (char c : jsonModel.toJSONString().toCharArray()) {
+                                        sb.append(String.valueOf((int) c));
+                                    }
+                                }
+                                String token = Demo.summary(sb.toString());
+                                if (!token.equals(webcode)) {
+                                    log.error("参数篡改" + jsonModel.toJSONString() + " ,之后的summary: " + token + ",前端summary: " + webcode);
+                                    ctx.setSendZuulResponse(false);
+                                    ctx.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
+                                    return false;
+                                }
+                            }
+                            ctx.setRequest(requestWrapper);
                         }
-                        String token = Demo.summary(sb.toString());
-                        if (!token.equals(webcode)) {
-                            log.error("参数篡改" + jsonModel.toJSONString() + " ,之后的summary: " + token + ",前端summary: " + webcode);
-                            ctx.setSendZuulResponse(false);
-                            ctx.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
-                            return false;
-                        }
-                    }
-                    ctx.setRequest(requestWrapper);
                 } else {
-                    String tokenStr = request.getParameter("summary");
                     String str = request.getQueryString();
                     if (str != null) {
                         MultisMap multiMap = new MultisMap();
@@ -97,9 +116,7 @@ public class zuulFilter extends ZuulFilter {
                         Map<String, Object> params = JSON.parseObject(jsonObject.toString(), LinkedHashMap.class);
                         JSONObject jsonModel = new JSONObject(true);
                         for (Map.Entry<String, Object> param : params.entrySet()) {
-                            if (!"summary".equals(param.getKey()) && !"signStr".equals(param.getKey())) {
                                 jsonModel.put(param.getKey(), param.getValue());
-                            }
                         }
                         if (jsonModel.size() != 0) {
                             StringBuilder sb = new StringBuilder();
@@ -109,8 +126,8 @@ public class zuulFilter extends ZuulFilter {
                                 }
                             }
                             String token = Demo.summary(sb.toString());
-                            if (!token.equals(tokenStr)) {
-                                log.error("参数篡改" + jsonModel.toJSONString() + " ,之后的summary: " + token + ",前端summary: " + tokenStr);
+                            if (!token.equals(webcode)) {
+                                log.error("参数篡改" + jsonModel.toJSONString() + " ,之后的summary: " + token + ",前端summary: " + webcode);
                                 ctx.setSendZuulResponse(false);
                                 ctx.setResponseStatusCode(HttpStatus.SC_UNAUTHORIZED);
                                 return false;
@@ -123,44 +140,15 @@ public class zuulFilter extends ZuulFilter {
         if ("true".equals(isUkey)) {
             RequestContext ctx = RequestContext.getCurrentContext();
             HttpServletRequest request = ctx.getRequest();
-            MyRequestWrapper requestWrapper = null;
-            if (request instanceof HttpServletRequest) {
-                requestWrapper = new MyRequestWrapper(request);
-                if ("POST".equals(request.getMethod().toUpperCase()) || "PUT".equals(request.getMethod().toUpperCase())) {
-                    String signStr = null;
-                    String webcode = null;
-
-                    String body = requestWrapper.getBody();
-                    Map<String, Object> paramBodyMap = new LinkedHashMap<>();
-                    if (!StringUtils.isEmpty(body)) {
-                        paramBodyMap = JSONObject.parseObject(body, LinkedHashMap.class);
-                    }
-                    for (Map.Entry<String, Object> param : paramBodyMap.entrySet()) {
-                        if ("signStr".equals(param.getKey())) {
-                            signStr = String.valueOf(param.getValue());
-                        }
-                        if ("summary".equals(param.getKey())) {
-                            webcode = String.valueOf(param.getValue());
-                        }
-                    }
-                    boolean status = Demo.verify(webcode, signStr);
-                    if (!status) {
-                        log.error("签名验证结果 - " + status);
-                        ctx.setSendZuulResponse(false);
-                        ctx.setResponseStatusCode(HttpStatus.SC_PAYMENT_REQUIRED);
-                        return false;
-                    }
-                    ctx.setRequest(requestWrapper);
-                } else {
-                    String signStr = request.getParameter("signStr");
-                    String plainText = request.getParameter("summary");
-                    boolean status = Demo.verify(plainText, signStr);
-                    if (!status) {
-                        log.error("签名验证结果 - " + status);
-                        ctx.setSendZuulResponse(false);
-                        ctx.setResponseStatusCode(HttpStatus.SC_PAYMENT_REQUIRED);
-                        return false;
-                    }
+            String signStr = request.getHeader("signStr") != null ? request.getHeader("signStr") : "";
+            String webcode = request.getHeader("summary") != null ? request.getHeader("summary") : "";
+            if (StringUtils.isNoneBlank(signStr)) {
+                boolean status = Demo.verify(webcode, signStr);
+                if (!status) {
+                    log.error("签名验证结果 - " + status);
+                    ctx.setSendZuulResponse(false);
+                    ctx.setResponseStatusCode(HttpStatus.SC_PAYMENT_REQUIRED);
+                    return false;
                 }
             }
         }
