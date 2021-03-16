1670c70cf826eebc81055a99046559670bc3a844
yuangang <yuangang@yijiahe.com>
2021-03-16 10:15:00 +0800

    需求[#24390]:SR-20210303-02-01 新增用户名不能包含admin、administrator，用户名只能新增不能修改,用户查询/应答报文不能携带密码信息 [修改内容]:  SR-20210303-02-01 新增用户名不能包含admin、administrator，用户名只能新增不能修改,用户查询/应答报文不能携带密码信息 [描述说明]: [问题原因]: [解决方法]: [测试建议]:
    Change-Id: I2e5f55f135190813b2997c224a1f9a4b4795e756
    Signed-off-by: yuangang <yuangang@yijiahe.com>

--- iot-center-platform/src/main/java/com/yjh/platform/module/device/entity/VoiceDeviceAllInfoDetail.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/device/entity/VoiceDeviceAllInfoDetail.java
@@ -9,6 +9,8 @@ import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Max;
+
 /**
  * @author lqh
  * @since 2021/3/2
@@ -18,22 +20,26 @@ import org.hibernate.validator.constraints.Length;
 public class VoiceDeviceAllInfoDetail{
 
     private static final long serialVersionUID = 1L;
-
+    @Max(value = 999999999999999999l)
     @ApiModelProperty(value = "声纹监控设备Id（默认为Ip地址）")
     private Long voiceDeviceId;
 
+    @Length(max = 255, message = "voiceDeviceName长度必须小于等于255")
     @ApiModelProperty(value = "声纹监控设备名称（）")
     private String voiceDeviceName;
 
+    @Max(value = 999999999999999999l)
     @ApiModelProperty(value = "变压器下面换流变的设备Id")
     private Long stdDeviceId;
 
+    @Length(max = 64, message = "deviceType长度必须小于等于64")
     @ApiModelProperty(value = "被监测的设备类型")
     private String deviceType;
 
-    @Length(max = 32, message = "configId长度必须小于等于32")
+    @Max(value = 999999999999999999l)
     private Long configId;
 
+    @Max(value = 999999999999999999l)
     @ApiModelProperty(value = "上级区域id")
     private Long upRegionId;
 
