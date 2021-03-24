790d0fb96423c2dde834341577eb7c096287f638
taotao <taotao@yijiahe.com>
2021-03-24 18:19:14 +0800

    需求[#31068]: SR-20210324-02-04 srs播流停止接口
        [修改内容]: SR-20210324-02-04 srs播流停止接口
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: If979240e05caad9af0451667e7ee3f3a10f852b6
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/controller/CameraConController.java
+++ iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/controller/CameraConController.java
@@ -94,16 +94,24 @@ public class CameraConController {
                 Integer clients = streambeanJson.getInteger("clients"); //观看人数
                 String app = streambeanJson.getString("app");//类型：直播 or 回放
 
-                if(app.equals("live") && !( active && clients==0)){
-                    //符合无人观看的条件
-                    cid = publishjson.getString("cid");
-                    //踢掉
-                    if(StringUtils.isNotEmpty(cid)){
-                        String delteUrl="http://"+srsStopUrl+":8082/api/v1/clients/"+cid;
-                        HttpClientUtils.httpDelete(delteUrl,null,null);
-                    }
-                    break;
+                //符合无人观看的条件
+                cid = publishjson.getString("cid");
+                //踢掉
+                if(StringUtils.isNotEmpty(cid)){
+                    String delteUrl="http://"+srsStopUrl+":8082/api/v1/clients/"+cid;
+                    HttpClientUtils.httpDelete(delteUrl,null,null);
                 }
+
+//                if(app.equals("live") && !active && clients==0){
+//                    //符合无人观看的条件
+//                    cid = publishjson.getString("cid");
+//                    //踢掉
+//                    if(StringUtils.isNotEmpty(cid)){
+//                        String delteUrl="http://"+srsStopUrl+":8082/api/v1/clients/"+cid;
+//                        HttpClientUtils.httpDelete(delteUrl,null,null);
+//                    }
+//                    break;
+//                }
             }
         }catch (Exception e){
             log.error("关流异常",e,this.getClass());
