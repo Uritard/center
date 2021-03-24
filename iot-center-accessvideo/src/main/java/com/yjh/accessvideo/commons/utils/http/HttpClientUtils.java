959a3f4bf056ca495b00d08cb13eeeab48329bd7
taotao <taotao@yijiahe.com>
2021-03-24 16:12:37 +0800

    需求[#31068]: SR-20210324-02-04 srs播流停止接口
        [修改内容]: SR-20210324-02-04 srs播流停止接口
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I92bd7047b270c8e230db2e30f8647f8d2b4dedbb
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-accessvideo/src/main/java/com/yjh/accessvideo/commons/utils/http/HttpClientUtils.java
+++ iot-center-accessvideo/src/main/java/com/yjh/accessvideo/commons/utils/http/HttpClientUtils.java
@@ -1,17 +1,18 @@
 package com.yjh.accessvideo.commons.utils.http;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.yjh.accessvideo.commons.result.BusinessException;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
+import org.apache.http.*;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
@@ -19,6 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -183,6 +185,93 @@ public class HttpClientUtils {
             }
         } catch (Exception e) {
             logger.error(e.getMessage());
+        }
+    }
+
+
+    private static final CloseableHttpClient httpclient;
+    static{
+        RequestConfig config = RequestConfig.custom().setConnectTimeout(5000).setSocketTimeout(3000).build();
+        httpclient = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+    }
+    public static JSONObject sendGet(String url, Map<String, Object> params) throws ParseException, UnsupportedEncodingException, IOException{
+
+        if(params !=null && !params.isEmpty()){
+
+            List<NameValuePair> pairs = new ArrayList<NameValuePair>(params.size());
+
+            for (String key :params.keySet()){
+                pairs.add(new BasicNameValuePair(key, params.get(key).toString()));
+            }
+            url +="?"+EntityUtils.toString(new UrlEncodedFormEntity(pairs), "UTF-8");
+        }
+
+        //请求超时时间设置
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(5000).setConnectionRequestTimeout(1000)
+                .setSocketTimeout(5000).build();
+
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.setConfig(requestConfig);
+
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        int statusCode = response.getStatusLine().getStatusCode();
+        if(statusCode !=200){
+            httpGet.abort();
+            throw new RuntimeException("HttpClient,error status code :" + statusCode);
+        }
+        HttpEntity entity = response.getEntity();
+        String result = null;
+        if (entity != null) {
+            result = EntityUtils.toString(entity, "utf-8");
+            EntityUtils.consume(entity);
+            response.close();
+            JSONObject resultJson = JSONObject.parseObject(result);
+            return resultJson;
+        }else{
+            return null;
+        }
+    }
+
+    /**
+     * 发送http delete请求
+     */
+    public static void httpDelete(String url, Map<String,String> headers, String encode){
+        if(encode == null){
+            encode = "utf-8";
+        }
+        String content = null;
+        //since 4.3 不再使用 DefaultHttpClient
+        CloseableHttpClient closeableHttpClient = HttpClientBuilder.create().build();
+        HttpDelete httpdelete = new HttpDelete(url);
+        //设置header
+        if (headers != null && headers.size() > 0) {
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                httpdelete.setHeader(entry.getKey(),entry.getValue());
+            }
+        }
+        CloseableHttpResponse httpResponse = null;
+        try {
+            httpResponse = closeableHttpClient.execute(httpdelete);
+            HttpEntity entity = httpResponse.getEntity();
+            content = EntityUtils.toString(entity, encode);
+//            response.setBody(content);
+//            response.setHeaders(httpResponse.getAllHeaders());
+//            response.setReasonPhrase(httpResponse.getStatusLine().getReasonPhrase());
+//            response.setStatusCode(httpResponse.getStatusLine().getStatusCode());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                httpResponse.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {   //关闭连接、释放资源
+            closeableHttpClient.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
--- iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/controller/CameraConController.java
+++ iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/controller/CameraConController.java
@@ -1,19 +1,26 @@
 package com.yjh.accessvideo.module.control.controller;
 
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.yjh.accessvideo.commons.result.BusinessException;
 import com.yjh.accessvideo.commons.result.Result;
 import com.yjh.accessvideo.commons.result.ResultCodeEnum;
+import com.yjh.accessvideo.commons.utils.http.HttpClientUtils;
 import com.yjh.accessvideo.hik.HCNetSDK;
 import com.yjh.accessvideo.module.control.service.CameraConService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -46,6 +53,9 @@ public class CameraConController {
     @Value("${nvr.capture.Preset}")
     private String capturePresetPath;//预置位路径
 
+    @Value("${srs.stop.url}")
+    private String srsStopUrl;//srs停止播流
+
     @ApiOperation(value = "相机播放")
     @RequestMapping(value = "/startRealPlay", method = RequestMethod.GET)
     public Result startRealPlay(@RequestParam(value = "cameraId") Long cameraId) {
@@ -59,6 +69,87 @@ public class CameraConController {
             log.error("相机播放失败:", e);
         }
         return result;
+    }
+
+    @ApiOperation(value = "视频进程关闭")
+    @RequestMapping(value = "/stopProcess", method = RequestMethod.POST)
+    public void  stopProcess(HttpServletRequest request, HttpServletResponse response){
+
+        try {
+            byte buffer[] = getRequestPostBytes(request);
+            String charEncoding = request.getCharacterEncoding();
+            if (charEncoding == null) {
+                charEncoding = "UTF-8";
+            }
+            String requestPostStr = new String(buffer, charEncoding);
+            JSONObject stopJson = null;
+            if (StringUtils.isNotEmpty(requestPostStr)) {
+                stopJson = (JSONObject) JSONObject.parse(requestPostStr);
+            }
+            String action = stopJson.getString("action");
+            if(StringUtils.isNotEmpty(action) && action.equals("on_stop")){
+                Thread.sleep(1000*70);//SRS服务器有延迟，大概50-60秒 才更新管理数据
+                String stream= stopJson.getString("stream");
+                String cid=null;
+                try{
+                    //获取所有视频流信息
+                    //发送请求获取所有STREAM
+                    String getInfoUrl="http://"+srsStopUrl+":8082/api/v1/streams/";
+                    JSONObject jsonList = HttpClientUtils.sendGet(getInfoUrl, null);
+                    List<String> streamsJsonObjectList = JSONArray.parseArray(jsonList.getString("streams"),String.class);
+                    for (int i = 0; i < streamsJsonObjectList.size(); i++) {
+                        String streambeanStr = streamsJsonObjectList.get(i);
+                        JSONObject streambeanJson = JSONObject.parseObject(streambeanStr);
+
+                        //解析每一个stream，循环比对，找到页面传递的设备ID对应的流，并判断是否需要关闭
+                        String name = streambeanJson.getString("name");//设备ID
+                        String publish = streambeanJson.getString("publish");
+                        JSONObject publishjson = JSONObject.parseObject(publish);
+                        Boolean active = publishjson.getBoolean("active");//是否有人观看
+                        Integer clients = streambeanJson.getInteger("clients"); //观看人数
+                        String app = streambeanJson.getString("app");//类型：直播 or 回放
+
+                        if(app.equals("live") && name.equals(stream) && !( active && clients>=2)){
+                            //符合无人观看的条件
+                            cid = publishjson.getString("cid");
+                            //踢掉
+                            if(StringUtils.isNotEmpty(cid)){
+                                String delteUrl="http://"+srsStopUrl+":8082/api/v1/clients/"+cid;
+                                HttpClientUtils.httpDelete(delteUrl,null,null);
+                            }
+                            break;
+                        }
+                    }
+                }catch (Exception e){
+                    log.error("关流异常",e,this.getClass());
+                }
+            }
+        }
+        catch (Exception e) {
+            log.error("关流异常",e,this.getClass());
+        }
+    }
+
+    /**
+     * 描述:获取 post 请求的 byte[] 数组
+     */
+    public static byte[] getRequestPostBytes(HttpServletRequest request)
+            throws IOException {
+        int contentLength = request.getContentLength();
+        if(contentLength<0){
+            return null;
+        }
+        byte buffer[] = new byte[contentLength];
+        for (int i = 0; i < contentLength;) {
+
+            int readlen = request.getInputStream().read(buffer, i,
+                    contentLength - i);
+            if (readlen == -1) {
+                break;
+            }
+            i += readlen;
+        }
+        return buffer;
     }
 
     @ApiOperation(value = "相机停止播放")
@@ -565,7 +656,6 @@ public class CameraConController {
         result.setData(cameraConService.getVidemoIntercomStatus(videoIntercomId));
         return result;
     }
-
 
     /*@ApiOperation(value = "门口机布防")
     @RequestMapping(value = "/doorMa", method = RequestMethod.GET)
