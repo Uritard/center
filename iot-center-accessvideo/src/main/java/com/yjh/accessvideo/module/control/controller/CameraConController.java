8a8e621640ba21eb1781e095dcdd0e6677c85f06
taotao <taotao@yijiahe.com>
2021-03-24 18:57:47 +0800

    需求[#31068]: SR-20210324-02-04 srs播流停止接口
        [修改内容]: SR-20210324-02-04 srs播流停止接口
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I82033bece9eda5a21ac731017015e742f0478103
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/controller/CameraConController.java
+++ iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/controller/CameraConController.java
@@ -87,31 +87,20 @@ public class CameraConController {
                 JSONObject streambeanJson = JSONObject.parseObject(streambeanStr);
 
                 //解析每一个stream，循环比对，找到页面传递的设备ID对应的流，并判断是否需要关闭
-                String name = streambeanJson.getString("name");//设备ID
                 String publish = streambeanJson.getString("publish");
                 JSONObject publishjson = JSONObject.parseObject(publish);
-                Boolean active = publishjson.getBoolean("active");//是否有人观看
                 Integer clients = streambeanJson.getInteger("clients"); //观看人数
-                String app = streambeanJson.getString("app");//类型：直播 or 回放
 
-                //符合无人观看的条件
-                cid = publishjson.getString("cid");
-                //踢掉
-                if(StringUtils.isNotEmpty(cid)){
-                    String delteUrl="http://"+srsStopUrl+":8082/api/v1/clients/"+cid;
-                    HttpClientUtils.httpDelete(delteUrl,null,null);
+                if(clients==0){
+                    //符合无人观看的条件
+                    cid = publishjson.getString("cid");
+                    //踢掉
+                    if(StringUtils.isNotEmpty(cid)){
+                        String delteUrl="http://"+srsStopUrl+":8082/api/v1/clients/"+cid;
+                        HttpClientUtils.httpDelete(delteUrl,null,null);
+                    }
+                    break;
                 }
-
-//                if(app.equals("live") && !active && clients==0){
-//                    //符合无人观看的条件
-//                    cid = publishjson.getString("cid");
-//                    //踢掉
-//                    if(StringUtils.isNotEmpty(cid)){
-//                        String delteUrl="http://"+srsStopUrl+":8082/api/v1/clients/"+cid;
-//                        HttpClientUtils.httpDelete(delteUrl,null,null);
-//                    }
-//                    break;
-//                }
             }
         }catch (Exception e){
             log.error("关流异常",e,this.getClass());
