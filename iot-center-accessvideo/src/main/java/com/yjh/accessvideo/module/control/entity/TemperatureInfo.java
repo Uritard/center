7caa6b18bee346b68666a88f512e48945525fc40
gengjingyang <gengjingyang@yijiahe.com>
2021-03-26 17:08:53 +0800

    修改[#20716]: SR-20200914-01-08修改坐标温度
        [修改内容]:  SR-20200914-01-08 修改坐标温度
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: Idc9a95f3485b87cf3b6b46b866accfb80fa807a2

---
+++ iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/entity/TemperatureInfo.java
@@ -0,0 +1,16 @@
+package com.yjh.accessvideo.module.control.entity;
+
+import lombok.Data;
+
+/**
+ * @author prozac.G
+ */
+@Data
+public class TemperatureInfo {
+
+    private String points;
+
+    private Long cameraId;
+
+    private  String  picPath;
+}
