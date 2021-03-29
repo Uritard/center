143f6df2d8b64b7c313961c0d0ca18ae777c70bc
taotao <taotao@yijiahe.com>
2021-03-29 18:02:59 +0800

    缺陷[#23420]: 【智能巡视-任务管理】点击日历查看任务数据，删除整个周期任务，提示系统异常，再次删除可正常删除
        [修改内容]: 【智能巡视-任务管理】点击日历查看任务数据，删除整个周期任务，提示系统异常，再次删除可正常删除
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: If8f3de6323579d70f1fe5bafbfd31ea3c6a0b625
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-gateway/src/main/java/com/yjh/gateway/common/authFilter/zuulFilter.java
+++ iot-center-gateway/src/main/java/com/yjh/gateway/common/authFilter/zuulFilter.java
@@ -9,7 +9,6 @@ import com.yjh.gateway.common.utils.Decode;
 import com.yjh.gateway.common.utils.GPSFormatUtils;
 import com.yjh.gateway.common.utils.IpUtil;
 import com.yjh.gateway.common.utils.MultisMap;
-import com.yjh.gateway.commons.utils.http.IPUtil;
 import com.yjh.gateway.commons.utils.smUtil.Demo;
 import lombok.SneakyThrows;
 import org.apache.commons.lang3.StringUtils;
@@ -64,7 +63,7 @@ public class zuulFilter extends ZuulFilter {
         if ("true".equals(isDecode)) {
             RequestContext ctx = RequestContext.getCurrentContext();
             HttpServletRequest request = ctx.getRequest();
-            String url = IPUtil.getRemoteIP(request);
+            String url = request.getRequestURI();
             log.info("url: "+request.getRequestURL());
             if (!url.contains("/sysUser/v1/login")) {
                 String userId = request.getHeader("userId") != null ? request.getHeader("userId") : "";
