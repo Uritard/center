0b509c1e2a7abdea34f38a0919f3d9f2b045d8e1
taotao <taotao@yijiahe.com>
2021-03-30 10:27:44 +0800

    缺陷[#26097]: 【安全-日志管理】日志管理中IP显示错误，应该为登录客户端的IP地址，当前是本服务器IP地址
        [修改内容]: 【安全-日志管理】日志管理中IP显示错误，应该为登录客户端的IP地址，当前是本服务器IP地址
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: Ic8647fc43afb6a83c034c64e503d2145420cd8b8
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-accessvideo/src/main/java/com/yjh/accessvideo/common/logs/LogsAspect.java
+++ iot-center-accessvideo/src/main/java/com/yjh/accessvideo/common/logs/LogsAspect.java
@@ -69,11 +69,7 @@ public class LogsAspect {
                 userName = String.valueOf(redisTemplate.opsForHash().entries("userInfo:"+userId).get("userName"));
             } else { userName = "admin"; }
         }
-        ip = request.getHeader("X-Real-IP");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { ip = request.getHeader("X-Forwarded-For"); }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { ip = request.getHeader("Proxy-Client-IP"); }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { ip = request.getHeader("WL-Proxy-Client-IP"); }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) { ip = request.getRemoteAddr(); }
+        ip = request.getHeader("HTTP_X_FORWARDED_FOR");
         Object result = null;
         if (annotation != null) {
             try {
