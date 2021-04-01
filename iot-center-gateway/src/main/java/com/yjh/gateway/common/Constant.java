517cd2db50047f6fd84845844b9013f215f208ea
yuangang <yuangang@yijiahe.com>
2021-04-01 17:52:11 +0800

    需求[#24811]:【登录】登录页面，输入错误的用户名，页面提示语为空 [修改内容]:  【登录】登录页面，输入错误的用户名，页面提示语为空 [描述说明]: [问题原因]: [解决方法]: [测试建议]:
    Change-Id: If9e35a60da874f02b0e469a5c267f3f01e365e67
    Signed-off-by: yuangang <yuangang@yijiahe.com>

--- iot-center-gateway/src/main/java/com/yjh/gateway/common/Constant.java
+++ iot-center-gateway/src/main/java/com/yjh/gateway/common/Constant.java
@@ -55,5 +55,5 @@ public class Constant {
 
     public static String isUkey;
 
-    public static final String LOGOUT_GATEWAY="http://127.0.0.1:18711/sysUser/v1/logoutGateway";
+    public static final String LOGOUT_GATEWAY="http://192.168.9.40:18711/sysUser/v1/logoutGateway";
 }
