814820e3259ecc2a997d20d2679da998c6f1b65e
taotao <taotao@yijiahe.com>
2021-04-03 10:49:45 +0800

    需求[#31068]: SR-20210324-02-04 srs播流停止接口
        [修改内容]: SR-20210324-02-04 srs播流停止接口
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: Ia3543ff942b97b66a9784127bc1bd89287f4d16a
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/service/CameraConService.java
+++ iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/service/CameraConService.java
@@ -176,6 +176,7 @@ public class CameraConService {
             //livePath
             String name = streambeanJson.getString("name");
             String videoFlowId = streambeanJson.getString("id");
+            log.info("realName: " + name);
             if (Objects.equals(name, String.valueOf(livePath)) && StringUtils.isNotEmpty(streambeanJson.getString("cid"))) {
                 returnMap.put("videoFlowId", videoFlowId);
                 Constant.mapsForCamera.put(videoFlowId, String.valueOf(cameraId));
@@ -287,6 +288,7 @@ public class CameraConService {
                     //livePath
                     String name = streambeanJson.getString("name");
                     String videoFlowId = streambeanJson.getString("id");
+                    log.info("realName: " + name);
                     if (Objects.equals(name, String.valueOf(livePath)) && StringUtils.isNotEmpty(streambeanJson.getString("cid"))) {
                         returnMap.put("videoFlowId", videoFlowId);
                         Constant.mapsForCamera.put(videoFlowId, String.valueOf(cameraId));
