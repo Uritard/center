0e7d274587dd67adbbfd7c08264a32b2b371bfad
taotao <taotao@yijiahe.com>
2021-04-03 17:38:19 +0800

    需求[#31068]: SR-20210324-02-04 srs播流停止接口
        [修改内容]: SR-20210324-02-04 srs播流停止接口
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I21a32a9f773a4ea3738ab185b96776ff1e454bfb
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/service/StreamStopThread.java
+++ iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/service/StreamStopThread.java
@@ -33,6 +33,8 @@ public class StreamStopThread implements Runnable {
     @Override
     public void run() {
         try {
+            //减少IO
+            Thread.sleep(60000);
             //解析每一个stream，循环比对，找到页面传递的设备ID对应的流，并判断是否需要关闭
             for (int i = 0; i < streamListSize; i++) {
                 String streambeanStr = streamsJsonObjectList.get(i);
