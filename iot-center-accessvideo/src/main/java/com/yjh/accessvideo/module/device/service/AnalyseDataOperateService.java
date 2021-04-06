9bed9b8b90e98a57adf7d39216f20d3496360396
changzuhao <changzuhao@yijiahe.com>
2021-04-06 16:22:49 +0800

    缺陷#[31187]:【告警确认-告警核查】告警核查中图片需要标注出具体缺陷或异常位置
        [描述说明]:红外测温补偿
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I54851a5867655234232d2e66d508ffce5df1ffc6

--- iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/device/service/AnalyseDataOperateService.java
+++ iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/device/service/AnalyseDataOperateService.java
@@ -704,5 +704,20 @@ public class AnalyseDataOperateService {
     public Long selectCurrentWarn(){
         return analyseDataOperateDao.selectCurrentWarn();
     }
+
+
+    /**
+     * 红外温度补偿
+     * @param value
+     * @return
+     */
+    public String thermalDataRevise(String value){
+        Double longValue=Double.valueOf(value);
+        Double  longRevise=(longValue/10);
+        String[] stringRevise=longRevise.toString().split("\\.");
+        Double  finalValue=longValue-Long.valueOf(stringRevise[0]);
+
+        return finalValue.toString();
+    }
 }
 
