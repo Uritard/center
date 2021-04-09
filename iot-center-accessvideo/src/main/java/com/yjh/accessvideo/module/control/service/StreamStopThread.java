df9a76bfa2a49a66271fbb90291a68f9090bf300
taotao <taotao@yijiahe.com>
2021-04-09 20:42:59 +0800

    缺陷[#26097]: 【安全-日志管理】日志管理中IP显示错误，应该为登录客户端的IP地址，当前是本服务器IP地址
        [修改内容]: 【安全-日志管理】日志管理中IP显示错误，应该为登录客户端的IP地址，当前是本服务器IP地址
        [描述说明]:
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I4cc43801d1d5fd8b8b96af1d072cd8bc28ced87d
    Signed-off-by: taotao <taotao@yijiahe.com>

--- iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/service/StreamStopThread.java
+++ iot-center-accessvideo/src/main/java/com/yjh/accessvideo/module/control/service/StreamStopThread.java
@@ -59,11 +59,11 @@ public class StreamStopThread implements Runnable {
                     log.info("{}流为空", videoFlowId);
                     for (String key:Constant.mapsForCamera.keySet()) {
                         log.info(key+" "+livePath);
-                        if (Objects.equals(Constant.mapsForCamera.get(key), livePath)) { Constant.mapsForCamera.remove(videoFlowId); }
+                        if (Objects.equals(Constant.mapsForCamera.get(key), livePath)) { Constant.mapsForCamera.remove(key); }
                     }
                     for (String key:Constant.mapsForHistory.keySet()) {
                         log.info(key+" "+livePath);
-                        if (Objects.equals(Constant.mapsForHistory.get(key), livePath)) { Constant.mapsForHistory.remove(videoFlowId); }
+                        if (Objects.equals(Constant.mapsForHistory.get(key), livePath)) { Constant.mapsForHistory.remove(key); }
                     }
                 } else if (clients>1){ log.info("{}流有人在看", videoFlowId); }
                 String url = "ps -ef | grep ffmpeg | grep '/" + livePath + "' | grep -v 'grep'";
