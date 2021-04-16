b0ff3651dc2f6d5c249195a9128c7e51796ada8c
lqh <liangqinghua@yijiahe.com>
2021-04-16 09:36:26 +0800

    需求[#32705]: SR-20210103-02-64 版本代码优化
        [描述说明]:  不写入日志优化
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I701546638f0409b4386d636ff75fba4c1af15b7c

--- iot-center-share/src/main/java/com/yjh/logs/common/utils/NumToStringUtil.java
+++ iot-center-share/src/main/java/com/yjh/logs/common/utils/NumToStringUtil.java
@@ -59,51 +59,57 @@ public class NumToStringUtil {
 
 
 
-//        if("0".equals(str)){
-//            return "保存";
-//        }
-//        if("1".equals(str)){
-//            return "查询";
-//        }
-//        if("2".equals(str)){
-//            return "新增";
-//        }
-//        if("3".equals(str)){
-//            return "修改";
-//        }
-//        if("4".equals(str)){
-//            return "删除";
-//        }
-//        if("5".equals(str)){
-//            return "确认";
-//        }
-//        if("6".equals(str)){
-//            return "登录";
-//        }
-//        if("7".equals(str)){
-//            return "登出";
-//        }
-//        if("8".equals(str)){
-//            return "导入";
-//        }
-//        if("9".equals(str)){
-//            return "导出";
-//        }
-//        if("10".equals(str)){
-//            return "任务下发";
-//        }
-//        if("11".equals(str)){
-//            return "任务暂停";
-//        }
-//        if("12".equals(str)){
-//            return "任务恢复";
-//        }
-//        if("13".equals(str)){
-//            return "任务终止";
-//        }
+        if("0".equals(str)){
+            return "保存";
+        }
+        if("1".equals(str)){
+            return "查询";
+        }
+        if("2".equals(str)){
+            return "新增";
+        }
+        if("3".equals(str)){
+            return "修改";
+        }
+        if("4".equals(str)){
+            return "删除";
+        }
+        if("5".equals(str)){
+            return "确认";
+        }
+        if("6".equals(str)){
+            return "登录";
+        }
+        if("7".equals(str)){
+            return "登出";
+        }
+        if("8".equals(str)){
+            return "导入";
+        }
+        if("9".equals(str)){
+            return "导出";
+        }
+        if("10".equals(str)){
+            return "任务下发";
+        }
+        if("11".equals(str)){
+            return "任务暂停";
+        }
+        if("12".equals(str)){
+            return "任务恢复";
+        }
+        if("13".equals(str)){
+            return "任务终止";
+        }
 //        if("14".equals(str)){
 //            return "全部";
 //        }
+        if("15".equals(str)){
+            return "检索";
+        }
+        if("16".equals(str)){
+            return "预览";
+        }
         return null;
     }
 
