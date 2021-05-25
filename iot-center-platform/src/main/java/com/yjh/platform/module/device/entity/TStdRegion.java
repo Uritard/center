9585b85019d5a3a709efc69bfab0c5baeabba348
lqh <liangqinghua@yijiahe.com>
2021-05-25 10:55:34 +0800

    需求[#34218]: 【大数据量】系统管理-配置管理-视频预置位配置，1w个预置位文件，全部导出，download接口超时
        [描述说明]:   系统管理-配置管理-视频预置位配置，1w个预置位文件，全部导出，download接口超时
        [问题原因]:
        [解决方法]:
        [测试建议]:
    
    Change-Id: I17c3b49a1e8cfb23f4357e44e71d627c24d7f9b7

--- iot-center-platform/src/main/java/com/yjh/platform/module/device/entity/TStdRegion.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/device/entity/TStdRegion.java
@@ -51,7 +51,7 @@ public class TStdRegion implements Serializable {
     @TableField(value = "up_region_ids",updateStrategy = FieldStrategy.IGNORED)
     private String upRegionIds;
 
-    @Length(max = 10,message = "upRegionIds长度必须小于等于10")
+    @Length(max = 10,message = "regionCode长度必须小于等于10")
     @ApiModelProperty(value = "类型区域，标准测点区域类型：100；E机器人区域类型：101；相机区域类型：102")
     @TableField(updateStrategy = FieldStrategy.IGNORED)
     private String regionCode;
