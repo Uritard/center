7245d9623c842fd04ee409d727311a8b66d54d7e
chenxiaoming <chenxiaoming@yijiahe.com>
2022-04-19 15:14:19 +0800

    REQ[#46408]: mqtt代码优化-clientid唯一
    [Description]: mqtt代码优化-clientid唯一
    
    Change-Id: Ib109e4fdcaf6fc6d2d106208bf5c071f10fa65bf
    Signed-off-by: chenxiaoming <chenxiaoming@yijiahe.com>

--- iot-center-platform/src/main/java/com/yjh/platform/configuration/MQTTConfig.java
+++ iot-center-platform/src/main/java/com/yjh/platform/configuration/MQTTConfig.java
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * <功能描述>
- *  mqtt配置
+ * mqtt配置
+ *
  * @author xmchen
  * @date 2022/4/18
  * @since [产品/模块版本] （可选）
@@ -17,15 +21,35 @@ public class MQTTConfig {
 
     @Value("${mqtt.host}")
     private String host;
-    @Value("${mqtt.serverClientId}")
-    private String serverClientId;
+    @Value("${spring.application.name}")
+    private String appName;
+    @Value("${server.port}")
+    private String appPort;
     @Value("${mqtt.user}")
     private String user;
     @Value("${mqtt.pwd}")
     private String pwd;
 
+    /**
+     * mqtt连接保证clientid唯一
+     * @return
+     */
     @Bean
     public MqttUtilsServer getMQTTServer() {
-        return new MqttUtilsServer(host, serverClientId, user, pwd);
+        return new MqttUtilsServer(host, getIp() + "#" + appName + "#" + appPort, user, pwd);
+    }
+
+    /**
+     * 获取ip地址
+     * @return
+     */
+    private String getIp() {
+        String ip = "0.0.0.0";
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return ip;
     }
 }
