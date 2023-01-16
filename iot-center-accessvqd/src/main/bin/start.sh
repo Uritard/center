eb45347869cb3fe8544453cd0d9ac71450c744e8
chenfei <chenfei@yijiahe.com>
2023-01-16 11:21:39 +0800

    REQ[#62082]: 启动脚本增加内存限制和GC日志
    [Description]: 1. 启动脚本增加内存限制和GC日志
    [Root Cause]:
    [Solution]:
    [Test Suggestion]:

--- iot-center-accessvqd/src/main/bin/start.sh
+++ iot-center-accessvqd/src/main/bin/start.sh
@@ -7,9 +7,13 @@ process_exists=`ps -ef|grep $latestjar|grep -v grep|awk '{print $2}'`
 if [ -n "${process_exists}" ];then
 	kill -9 ${process_exists}
 fi
+
+JAVA_OPTS="-server -Xms1024m -Xmx1024m -XX:MetaspaceSize=128m -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75"
+JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:$SH_DIR/logs/verbose.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=200M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$SH_DIR/heap_%p_%t.dump"
+
 if [ ! -n "$java_home" ]; then
         cd $SH_DIR/../;
-        nohup java -jar $SH_DIR/../$latestjar > /dev/null 2>&1 &
+        nohup java $JAVA_OPTS -jar $SH_DIR/../$latestjar > /dev/null 2>&1 &
 	echo " application is starting......";
         cd -;
         exit;
@@ -17,7 +21,7 @@ else
 	java -version;
         cd $SH_DIR/../;
 	if [ $? -eq 0]; then
-		nohup java -jar $SH_DIR/../$latestjar > /dev/null 2>&1 &
+		nohup java $JAVA_OPTS -jar $SH_DIR/../$latestjar > /dev/null 2>&1 &
 	else	
 		echo "this system has no jdk";
                 cd -;
