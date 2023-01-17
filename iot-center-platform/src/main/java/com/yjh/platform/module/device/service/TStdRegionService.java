927098a4a6522e7cd225aea72a5c1f4c09723935
chenfei <chenfei@yijiahe.com>
2023-01-17 16:39:21 +0800

    REQ[#62445]: 场站区域编码不判断是否为空
    [Description]: 1. 新增场站区域编码不判断是否为空
    2. 修改场站区域编码只与本级系统判断是否重复
    [Root Cause]:
    [Solution]:
    [Test Suggestion]:
    
    Change-Id: I08c519b0a2edd1feaa9e681bc56e7bdad3ae6b55

--- iot-center-platform/src/main/java/com/yjh/platform/module/device/service/TStdRegionService.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/device/service/TStdRegionService.java
@@ -36,17 +36,11 @@ public class TStdRegionService{
 
     @Transactional(rollbackFor = Exception.class)
     public int insert(TStdRegion tStdRegion) {
-        //区域编码非空校验
-        if (StringUtils.isBlank(tStdRegion.getRegionCode())) {
-            throw new BusinessException(201, "区域编码不可为空");
-        }
-        //判断父节点是否为根节点，根节点下的2层节点，区域编码不可重复
-        TStdRegion tStdRegionParent = tStdRegionDao.selectByPrimaryId(tStdRegion.getUpRegionId());
-        if (Objects.nonNull(tStdRegionParent) && tStdRegionParent.getUpRegionId() == -1) {
-            Integer count = this.countByRegionCode(tStdRegion.getRegionCode(), tStdRegion.getUpRegionId());
-            if (count > 0) {
-                throw new BusinessException(201, "区域编码" + tStdRegion.getRegionCode() + "已存在");
-            }
+
+        // 判断当前区域编码是否与本级系统的区域编码重复
+        Integer count = this.countByRegionCode(tStdRegion.getRegionCode(), null);
+        if (count > 0) {
+            throw new BusinessException(201, "区域编码" + tStdRegion.getRegionCode() + "已存在");
         }
         return this.tStdRegionDao.insert(tStdRegion);
     }
@@ -67,7 +61,7 @@ public class TStdRegionService{
     @Transactional(rollbackFor = Exception.class)
     public int update(TStdRegion tStdRegion) {
         String regionCode = tStdRegion.getRegionCode();
-        if (!Objects.equals("", regionCode) && Objects.nonNull(regionCode)) {
+        if (StringUtils.isNotEmpty(regionCode)) {
             List<TStdRegion> list = tStdRegionDao.selectIsIn(tStdRegion);
             if(list != null && list.size()>0){
                 throw new BusinessException(209,"编码与其他厂站区域重复");
@@ -217,7 +211,7 @@ public class TStdRegionService{
         return tStdRegionDao.selectStationVoltageData();
     }
 
-    //判断当前区域编码是否在第2层节点已存在
+    // 判断当前区域编码是否在本级已经存在
     public Integer countByRegionCode(String regionCode, Long upRegionId) {
         return tStdRegionDao.countByRegionCode(regionCode, upRegionId);
     }
