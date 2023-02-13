554c9ba7e0f24a2ad9b4294fe5f105d5bbc1f32b
lqh <liangqinghua@yijiahe.com>
2023-02-13 14:32:15 +0800

    缺陷[#63859]: 220kv检测问题】巡视系统不支持智能摄像机静默监视秒级识别
    [描述说明]: 秒级静默监视调试.
    [问题原因]:
    [解决方法]:
    [测试建议]:

--- iot-center-platform/src/main/java/com/yjh/platform/context/Context.java
+++ iot-center-platform/src/main/java/com/yjh/platform/context/Context.java
@@ -1,11 +1,17 @@
 package com.yjh.platform.context;
 
 import com.yjh.messager.api.socket.BaseSocketServer;
+import com.yjh.platform.common.Constant;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 @Component
@@ -13,8 +19,11 @@ public class Context {
 
     private final BaseSocketServer socketServer;
 
-    public Context(BaseSocketServer socketServer) {
+    private final RedisTemplate redisTemplate;
+
+    public Context(BaseSocketServer socketServer,RedisTemplate redisTemplate) {
         this.socketServer = socketServer;
+        this.redisTemplate = redisTemplate;
     }
 
     @EventListener(ApplicationReadyEvent.class)
@@ -22,10 +31,19 @@ public class Context {
         log.warn("<========== app ready ==========>");
         try {
             socketServer.start();
+            deleteSecondSilentRedisConf();
         } catch (Exception e) {
             throw new RuntimeException("Socket服务启动失败", e);
         }
 
+    }
+
+    private void deleteSecondSilentRedisConf(){
+        String key = Constant.SILENT_SECOND+"**";
+        Set<String> keys = redisTemplate.keys(key);
+        if (CollectionUtils.isNotEmpty(keys)) {
+            redisTemplate.delete(keys);
+        }
     }
 
     @EventListener
