53267176e3ac662c1dccd2877fa4b304488e818b
jinyujiang <jinyujiang@yijiahe.com>
2023-03-01 10:28:26 +0800

    REQ[#63874] 预置位从巡视系统向边缘节点同步，增加图片采集日志 [Description] 预置位从巡视系统向边缘节点同步，增加图片采集日志

--- iot-center-platform/src/main/java/com/yjh/platform/module/user/controller/TCameraPresetController.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/user/controller/TCameraPresetController.java
@@ -476,6 +476,7 @@ public class TCameraPresetController {
             if (!StringUtils.isEmpty(response1.getData().toString())) {
                 Result response2 = sendPostRequest(Constant.CAPTURE_PRESET_URL, params);//预置位抓图
                 JSONObject json = (JSONObject)JSON.toJSON(response2.getData());
+                log.info("重置预置位相机抓图结果：{}", response2.getData());
 
                 String realLocalPath = (String)json.get("urlPath");
                 realLocalPath = realLocalPath.replaceAll(redisTemplate.opsForHash().get("t_sys_param:presetRealImgPath","content").toString(), redisTemplate.opsForHash().get("t_sys_param:presetImgPath","content").toString());
