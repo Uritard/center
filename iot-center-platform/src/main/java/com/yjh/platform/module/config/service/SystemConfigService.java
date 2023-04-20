bc375e358ddf64a8d46500aff2c2106479f6da0c
lqh <liangqinghua@yijiahe.com>
2023-04-20 10:42:04 +0800

    需求[#67415]: 【系统优化】统一配置管理
    [描述说明]: 统一配置管理.
    [问题原因]:
    [解决方法]:
    [测试建议]:

--- iot-center-platform/src/main/java/com/yjh/platform/module/config/service/SystemConfigService.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/config/service/SystemConfigService.java
@@ -2,12 +2,17 @@ package com.yjh.platform.module.config.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yjh.platform.configuration.ApplicationProperties;
 import com.yjh.platform.module.config.dao.SystemConfigDao;
 import com.yjh.platform.module.config.entity.ConfigTreeNode;
 import com.yjh.platform.module.config.entity.SystemConfig;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -22,8 +27,18 @@ public class SystemConfigService {
 
     private final SystemConfigDao systemConfigDao;
 
-    public SystemConfigService(SystemConfigDao systemConfigDao) {
+    private final ApplicationProperties applicationProperties;
+
+    private final RedisTemplate redisTemplate;
+
+    private final String systemConfigKey="systemConfigKey:";
+
+    public SystemConfigService(SystemConfigDao systemConfigDao,
+                               ApplicationProperties applicationProperties,
+                               RedisTemplate redisTemplate) {
         this.systemConfigDao = systemConfigDao;
+        this.applicationProperties = applicationProperties;
+        this.redisTemplate = redisTemplate;
     }
 
     public List<ConfigTreeNode> selectConfigInfo() {
@@ -31,16 +46,27 @@ public class SystemConfigService {
     }
 
     public int update(SystemConfig systemConfig) {
+        List<SystemConfig> systemConfigList = new ArrayList<>();
+        systemConfigList.add(systemConfig);
+        systemConfigInfoToRedis(systemConfigList);
         flushCatch();
         return systemConfigDao.update(systemConfig);
     }
     public int batchUpdate(List<SystemConfig> systemConfig) {
-        flushCatch();
+        systemConfigInfoToRedis(systemConfig);
         return systemConfigDao.batchUpdate(systemConfig);
     }
 
-    public void flushCatch() {
+    private void systemConfigInfoToRedis(List<SystemConfig> configList){
+        configList.forEach(systemConfig ->{
+            Map<String,String> map = new HashMap<>();
+            map.put(systemConfig.getConfigKey(),systemConfig.getConfigValue());
+            redisTemplate.opsForHash().putAll(systemConfigKey+systemConfig.getConfigType(),map);
+        });
+    }
 
+    public void flushCatch() {
+        applicationProperties.flush();
     }
 
     public List<SystemConfig> selectAll() {
