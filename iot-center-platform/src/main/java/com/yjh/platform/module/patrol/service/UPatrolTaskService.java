a863e9178430a5e09563a9cc868d4486c657c0a7
huyuhang <huyuhang@yijiahe.com>
2023-05-19 13:55:40 +0800

    DEF[#69020]: 【任务审核】审核巡视值带单位的巡视点，platform报错
    [Description]: 审核巡视值带单位的巡视点，platform报错
    [Root Cause]: 巡视结果带单位无法解析数据，导致类型转换错误
    [Solution]: 去掉单位进行计算
    [Test Suggestion]:正常

--- iot-center-platform/src/main/java/com/yjh/platform/module/patrol/service/UPatrolTaskService.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/patrol/service/UPatrolTaskService.java
@@ -2116,6 +2116,7 @@ public class UPatrolTaskService {
                 uPatrolDataResult.setCruiseStatus(NumberUtils.toInt(redisInfoMap.get("cruiseStatus")));
                 uPatrolDataResult.setResultNum(redisInfoMap.get("resultNum"));
                 uPatrolDataResult.setResultDesc(redisInfoMap.get("resultDesc"));
+                uPatrolDataResult.setUnit(redisInfoMap.get("unit"));
                 uPatrolDataResult.setPicpath(redisInfoMap.get("picpath"));
                 uPatrolDataResult.setCruiseType(NumberUtils.toInt(redisInfoMap.get("cruiseType")));
                 uPatrolDataResult.setOrigpic(redisInfoMap.get("origpic"));
