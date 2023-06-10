cce40317c5083abe234747c47ab0cb9499542769
yangchen <yangchen@yijiahe.com>
2023-06-10 18:44:38 +0800

    DEF[#70540]: 一键顺控修改
     [Description]:
     1.一键顺控修改
    
    Signed-off-by: yangchen <yangchen@yijiahe.com>

--- iot-center-platform/src/main/java/com/yjh/platform/module/user/service/TSequentialConfService.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/user/service/TSequentialConfService.java
@@ -591,7 +591,19 @@ public class TSequentialConfService{
 
     @Transactional(rollbackFor = Exception.class)
     public List<Map<String,Object>> sequentialInfo(String cfgDeviceId){
-        return tSequentialConfDao.selectForSequenceInfo(cfgDeviceId);
+        List<Map<String, Object>> list = tSequentialConfDao.selectForSequenceInfo(cfgDeviceId);
+
+        Integer step = (Integer) Constant.sequentialState.get("state");
+        if(-1 == step) {
+            list.get(0).put("state", -1);
+            return list;
+        } else {
+            if (StringUtils.isEmpty(cfgDeviceId)) {
+                list.get(0).put("state", 1);
+                return list;
+            }
+        }
+        return list;
     }
     @Transactional(rollbackFor = Exception.class)
     public String unionTask(String cfgDeviceId,String order){
