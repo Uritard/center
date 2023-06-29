a75df6565c4996d6a3571da1c9f47bf940f9441b
huyuhang <huyuhang@yijiahe.com>
2023-06-29 08:54:43 +0000

    Merge branch 'bugfix-71181' into 'dev-changzhou-t'
    DEF[#71181]:【常州T版本】摄像机导入：相机区域或其他信息不正确的不可导入，应该不影响其他设备导入，与需求描述不一致
    
    See merge request iotcenter/IotCenter!1286
    
    (cherry picked from commit 57a24e2095772ed0e429d5c1c8ab216d44015894)
    
    b03e70a2 DEF[#71181]:【常州T版本】摄像机导入：相机区域或其他信息不正确的不可导入，应该不影响其他设备导入，与需求描述不一致

--- iot-center-platform/src/main/java/com/yjh/platform/module/user/controller/TCameraRecorderController.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/user/controller/TCameraRecorderController.java
@@ -12,7 +12,7 @@ import com.yjh.platform.common.restTemplate.ServiceRestTemplate;
 import com.yjh.platform.common.result.BusinessException;
 import com.yjh.platform.common.result.Result;
 import com.yjh.platform.common.result.ResultCodeEnum;
-import com.yjh.platform.common.utils.CameraRecordModelExcelListener;
+import com.yjh.platform.common.utils.ExcelReadListener;
 import com.yjh.platform.module.user.entity.TCameraRecorder;
 import com.yjh.platform.module.user.entity.TCameraRecorderByDict;
 import com.yjh.platform.module.user.entity.TCameraRecorderExcel;
@@ -297,15 +297,22 @@ public class TCameraRecorderController {
         Result result = new Result();
         try {
             InputStream inputStream = file.getInputStream();
-            CameraRecordModelExcelListener cameraRecordModelExcelListener = new CameraRecordModelExcelListener();
+            String[] heads = new String[] {"设备名称", "PMS编码", "设备别名", "设备类型", "设备型号", "生产厂家", "使用单位", "IP",
+                    "服务端口", "RTSP端口", "传输协议", "协议路径", "用户名", "密码", "最大通道数", "缓存天数", "缓存大小", "录制时长"};
+            ExcelReadListener<TCameraRecorderExcel> modelExcelListener = new ExcelReadListener<>(heads);
             ReadSheet readSheet = new ReadSheet(0);
-            EasyExcelFactory.read(inputStream, TCameraRecorderExcel.class,cameraRecordModelExcelListener).headRowNumber(1).build().read(readSheet);
-            List<TCameraRecorderExcel> excelEntities = cameraRecordModelExcelListener.getExcelEntities();
+            EasyExcelFactory.read(inputStream, TCameraRecorderExcel.class,modelExcelListener).headRowNumber(1).build().read(readSheet);
+            List<TCameraRecorderExcel> excelEntities = modelExcelListener.getExcelEntities();
+            List<String> errorExcelList = modelExcelListener.getErrorExcelEntities();
+
             if (CollectionUtils.isNotEmpty(excelEntities)) {
                 List<TCameraRecorder> tCameraRecorders = tCameraRecorderService.importCameraRecorder(excelEntities);
                 tCameraRecorders.forEach(t -> {
                     sendPostRequest(Constant.NVR_REGISTER_URL,t.getRecordId());
                 });
+            }
+            if (CollectionUtils.isNotEmpty(errorExcelList)) {
+                result.setData(ExcelReadListener.prettyErrors(errorExcelList));
             }
         } catch (Exception e) {
             result.setMessage(ResultCodeEnum.SYSTEMERROR.getCode(), e.getMessage());
