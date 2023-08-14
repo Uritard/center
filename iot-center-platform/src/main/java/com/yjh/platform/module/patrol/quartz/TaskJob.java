aadf4bcaecb979e2dc66dc2ffe47d3fb6197029e
huyuhang <huyuhang@yijiahe.com>
2023-08-14 14:23:40 +0800

    DEF[#74378]:【上级系统】已超过结束时间的周期任务，到了执行时间，给上级系统上报自定义41报文
    [Description]:已超过结束时间的周期任务，到了执行时间，给上级系统上报自定义41报文
    [Root Cause]: 已超过结束时间的周期任务 删除逻辑未删除改任务 导致一致重复删除
    [Solution]: 已超过结束时间的周期任务到点删除
    [Test Suggestion]:正常

--- iot-center-platform/src/main/java/com/yjh/platform/module/patrol/quartz/TaskJob.java
+++ iot-center-platform/src/main/java/com/yjh/platform/module/patrol/quartz/TaskJob.java
@@ -101,7 +101,7 @@ public class TaskJob extends QuartzJobBean {
             } else if (Objects.nonNull(ancestralTask.getEndTime()) && date.after(ancestralTask.getEndTime())) {
                 log.info("此时间大于结束时间,以后都不会再做了,删除当前任务:{}", taskId);
                 String edgeLevel = String.valueOf(redisTemplate.opsForHash().get("t_sys_param:edgeLevel", "content"));
-                uPatrolTaskService.deleteByPrimaryId(taskId, DateTimeUtil.format(date),edgeLevel, null);
+                uPatrolTaskService.deleteByPrimaryId(taskId, "-1", edgeLevel, null);
                 return;
             } else if (Objects.nonNull(ancestralTask.getStartTime()) && date.before(ancestralTask.getStartTime())) {
                 log.info(taskDate + "此时间小于开始时间,任务不需要执行:{}", taskId);
