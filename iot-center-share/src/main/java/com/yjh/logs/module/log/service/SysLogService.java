bfc15561837b6eda964620e172ac9c9c69d9d7d1
huyuhang <huyuhang@yijiahe.com>
2024-01-19 15:10:41 +0800

    DEF[#82481]: 【告警屏蔽】环控设备告警弹框，设置告警屏蔽，再次收到仍会弹框
    [Description]:环控设备告警弹框，设置告警屏蔽，再次收到仍会弹框
    [Root Cause]: 环控设备告警的告警屏蔽功能没有实现
    [Solution]: 添加告警屏蔽
    [Test Suggestion]:正常

--- iot-center-share/src/main/java/com/yjh/logs/module/log/service/SysLogService.java
+++ iot-center-share/src/main/java/com/yjh/logs/module/log/service/SysLogService.java
@@ -173,7 +173,6 @@ public class SysLogService {
 
     //请求webSocket发送方法
     public String postUrl(String json) throws IOException, URISyntaxException {
-        //  String url = redisTemplate.opsForHash().get("t_sys_param:webSocketUrl", "content").toString();
         CloseableHttpClient client = HttpClients.createDefault();
         URI uri = new URIBuilder(WEBSOCKET_SEND_URL).setParameter("json", json).build();
         HttpPost httpPost = new HttpPost(uri);
