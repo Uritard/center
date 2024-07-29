b21a6acbb6191cfce2ede2ff71a9dbfe45d43946
chenfei <chenfei@yijiahe.com>
2024-07-29 11:30:27 +0800

    REQ[#90619,90635,92675,90676]: 102 联动任务 t_p_id 为空无法正确解析
    [Description]:1. 机器人上报 task_patrolled_id 为空，则巡视上报也为空
    [Root Cause]:
    [Solution]:
    [Test Suggestion]:
    
    Change-Id: I9e6dd69d0420c50fbea1ded1b1e8f885372253db
    Signed-off-by: chenfei <chenfei@yijiahe.com>

--- iot-center-accessrobot/src/main/java/com/yjh/accessrobot/netty/handler/ModelFileSyncAndTaskControlHandler.java
+++ iot-center-accessrobot/src/main/java/com/yjh/accessrobot/netty/handler/ModelFileSyncAndTaskControlHandler.java
@@ -87,19 +87,22 @@ public class ModelFileSyncAndTaskControlHandler implements MessageHandlerStrateg
                         taskId = faultTolerantId;
                     }
 
-                    String taskPatrolledId = "";
+                    String taskPatrolledId = MapUtils.getString(resultMap, "task_patrolled_id");
                     if (StringUtils.isNotEmpty(errorCode)) {
-                        try {
-                            //根据taskCode找到taskId
-                            UPatrolTask task = robotService.selectTaskByTaskCode(taskId);
-                            String stationCode = (String) redisTemplate.opsForHash().get("t_sys_param:edgeId", "content");
-                            String taskStartTime = (String) redisTemplate.opsForHash().get(countForAbnormalKey+task.getTaskId(), "taskStart");
-                            taskPatrolledId = stationCode+"_"+taskId+"_"+DateTimeUtil.format(DateTimeUtil.getDate(taskStartTime), DateTimeUtil.getDateTimePattern3());
-                            log.info("本级上报的taskPatrolledId：{}", taskPatrolledId);
-                            xmlBaseModel.getItems().get(0).put("task_patrolled_id",taskPatrolledId);
-                        }catch (Exception e){
-                            log.info("构造本级taskPatrolledId出错：",e);
+                        if (StringUtils.isNotEmpty(taskPatrolledId)) {
+                            try {
+                                //根据taskCode找到taskId
+                                UPatrolTask task = robotService.selectTaskByTaskCode(taskId);
+                                String stationCode = (String) redisTemplate.opsForHash().get("t_sys_param:edgeId", "content");
+                                String taskStartTime = (String) redisTemplate.opsForHash().get(countForAbnormalKey+task.getTaskId(), "taskStart");
+                                taskPatrolledId = stationCode+"_"+taskId+"_"+DateTimeUtil.format(DateTimeUtil.getDate(taskStartTime), DateTimeUtil.getDateTimePattern3());
+                                log.info("本级上报的taskPatrolledId：{}", taskPatrolledId);
+                                xmlBaseModel.getItems().get(0).put("task_patrolled_id",taskPatrolledId);
+                            }catch (Exception e){
+                                log.info("构造本级taskPatrolledId出错：",e);
+                            }
                         }
+
                         log.info("机器人收到{}了,这是机器人响应的巡视任务执行Id==={}", taskMsg, taskPatrolledId);
                         // 联动的返回结果直接向上反
                         String success = "0";
