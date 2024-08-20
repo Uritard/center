21d3f4f643b6945929a0f0f714b62487ae5177bd
chenfei <chenfei@yijiahe.com>
2024-08-20 15:55:12 +0800

    REQ[#91372]: 启动及重启脚本修改，避免版本号更新后使用脚本重启失败
    [Description]:1. 启动及重启脚本修改，避免版本号更新后使用脚本重启失败
    [Root Cause]:
    [Solution]:
    [Test Suggestion]:
    
    Change-Id: Ib39d872c65e048fea8e8157ae6cb99e24d4e2fa6
    Signed-off-by: chenfei <chenfei@yijiahe.com>

--- iot-center-share/src/main/bin/start.sh
+++ iot-center-share/src/main/bin/start.sh
@@ -4,7 +4,9 @@ SH_DIR=${PWD%/*}
 java_home=env|grep "JAVA_HOME";
 latestjar=`ls $SH_DIR/ -lt | grep '.jar'| awk '{print $9}' | sed -n '1p'`
 echo 'start:'$latestjar
-process_exists=`ps -ef|grep $latestjar|grep -v grep|awk '{print $2}'`
+stopjar=`echo $latestjar |cut -d"-" -f1,2,3`
+process_exists=`ps -ef|grep -E ${stopjar}'\S+\.jar'|grep -v grep|awk '{print $2}'`
+echo 'stop process:'$stopjar'  '$process_exists
 if [ -n "${process_exists}" ];then
     kill -9 ${process_exists}
 fi
