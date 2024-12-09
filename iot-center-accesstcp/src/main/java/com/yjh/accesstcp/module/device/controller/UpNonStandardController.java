333c610a641393a0b6f18d1fb1506a6c53e71da2
chenfei <chenfei@yijiahe.com>
2024-12-09 09:28:03 +0800

    REQ[#93928]: 接入科大上级，上线下发任务逻辑实现
    [Description]: 1. 接入科大上级，上线下发任务转为电网接口执行任务
    [Root Cause]:
    [Solution]:
    [Test Suggestion]:
    
    Change-Id: Ib4d138e3161ea04f28d29e5827d113ecfd8c5cdd
    Signed-off-by: chenfei <chenfei@yijiahe.com>

---
+++ iot-center-accesstcp/src/main/java/com/yjh/accesstcp/module/device/controller/UpNonStandardController.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2024 Yijiahe Technology Co., Ltd. All rights reserved.
+ */
+
+package com.yjh.accesstcp.module.device.controller;
+
+import com.yjh.accesstcp.commons.result.BusinessException;
+import com.yjh.accesstcp.commons.result.Result;
+import com.yjh.accesstcp.module.device.service.uphandler.tek.TekUpTransforServer;
+import com.yjh.accesstcp.module.device.service.uphandler.tek.entity.TaskPlan;
+import com.yjh.accesstcp.module.device.service.uphandler.tek.entity.TekResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 非标准的上级系统接口处理
+ * @author Chenfei
+ * @date 2024/12/6
+ * @since [产品/模块版本] （可选）
+ */
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/")
+@Api(value = "/", tags = "非标准的上级系统接口处理")
+public class UpNonStandardController {
+    private final TekUpTransforServer tekUpTransforServer;
+
+    @ApiOperation(value = "任务下发")
+    @PostMapping(value = "/ioms/download")
+    public Result sendTask(@RequestBody TaskPlan taskPlan) {
+        TekResult result = new TekResult();
+        try {
+            tekUpTransforServer.sendTask(taskPlan);
+            result.success();
+        } catch (BusinessException e) {
+            result.error(e.getMessage());
+            log.error("下发任务错误: {}", e.getMessage(), e);
+        } catch (Exception e) {
+            result.error("任务下发失败，系统错误");
+            log.error("失败查询描述：", e);
+        }
+        return result;
+    }
+}
